ff3fb33325859a085ac3657bbe5186bc2f24be6c
Jeff McWhirter <jeffmc@unavco.org>
2007-08-31 13:42:03 +0000

    Don't use the old setDataCategories method

--- ucar/unidata/data/DerivedDataChoice.java
+++ ucar/unidata/data/DerivedDataChoice.java
@@ -525,7 +525,7 @@ public class DerivedDataChoice extends DataChoice {
                 }
             }
         }
-        setDataCategories(dataCategories);
+        setCategories(dataCategories);
     }
 
 
