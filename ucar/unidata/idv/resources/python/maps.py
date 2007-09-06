d9c7419a45ee9080d46bb55a585acc2c34bffe22
Jeff McWhirter <jeffmc@unavco.org>
2007-09-06 20:53:20 +0000

    Handle non time fields

--- ucar/unidata/idv/resources/python/maps.py
+++ ucar/unidata/idv/resources/python/maps.py
@@ -4,74 +4,79 @@
 #######  Map related utilities
 ####################################################################################
 
+import ucar.unidata.data.grid.GridUtil as gu
+
 
 def  subsetFromMap(field, map, fillValue=java.lang.Float.NaN,inverse=0,latLonCanChangeWithTime=1):
-   import ucar.unidata.data.grid.GridUtil as gu
-   newData = field.clone()
 ##Iterate on each time step
-   for timeStep in range(field.getDomainSet().getLength()):
-        rangeObject = field.getSample(timeStep).clone()
-##Find the indices of the lat lons that are inside the maps
-        if(timeStep == 0 or latLonCanChangeWithTime):
-           indices = gu.findIndices(rangeObject.getDomainSet(), map);
+    if (gu.isTimeSequence(field)):
+        newData = field.clone()
+        for timeStep in range(field.getDomainSet().getLength()):
+            rangeObject = subsetRangeFromMap(field.getSample(timeStep), timeStep, map, fillValue, inverse); 
+            newData.setSample(timeStep,rangeObject)
+        return newData
+    return subsetRangeFromMap(field, 0, map, fillValue, inverse);    
 
-##Get the values
-        originalValues = rangeObject.getFloats(0)
-        if(inverse):
-           newValues = originalValues;
-        else:
-           newValues = makeFloatArray(len(originalValues), len(originalValues[0]), fillValue);
 
+def  subsetRangeFromMap(range, timeStep, map, fillValue=java.lang.Float.NaN,inverse=0):
+    rangeObject = range.clone()
+    indices = gu.findContainedIndices(rangeObject.getDomainSet(), map);
+    originalValues = rangeObject.getFloats(0)
+    if(inverse):
+        newValues = originalValues;
+    else:
+        newValues = makeFloatArray(len(originalValues), len(originalValues[0]), fillValue);
 ##Look at list of indices in each map
 ##if we are doing inverse then we set the index to the fill value
 ##else we already filled the array and we set the index to the original value
-        for mapIdx in xrange(len(indices)):
-            indexArray = indices[mapIdx]
-            for j in xrange(len(indexArray)):
-               if(inverse):
-                  newValues[0][indexArray[j]] = fillValue
-               else:
-                  newValues[0][indexArray[j]] = originalValues[0][indexArray[j]];
-
+    for mapIdx in xrange(len(indices)):
+        indexArray = indices[mapIdx]
+        for j in xrange(len(indexArray)):
+           if(inverse):
+               newValues[0][indexArray[j]] = fillValue
+           else:
+               newValues[0][indexArray[j]] = originalValues[0][indexArray[j]];
 ##Set the samples
-        rangeObject.setSamples(newValues)
-        newData.setSample(timeStep,rangeObject)
-   return newData
+    rangeObject.setSamples(newValues)
+    return rangeObject;
 
 
 
 def  averageFromMap(field, map,latLonCanChangeWithTime=1):
-   import ucar.unidata.data.grid.GridUtil as gu
-   newData = field.clone()
-##Iterate on each time step
-   for timeStep in range(field.getDomainSet().getLength()):
-        rangeObject = field.getSample(timeStep).clone()
-##Find the indices of the lat lons that are inside the maps
-        if(timeStep == 0 or latLonCanChangeWithTime):
-            indices = gu.findIndices(rangeObject.getDomainSet(), map);
+    if (gu.isTimeSequence(field)):
+        newData = field.clone()
+        for timeStep in range(field.getDomainSet().getLength()):
+            rangeObject = averageRangeFromMap(field.getSample(timeStep), timeStep, map);
+            newData.setSample(timeStep,rangeObject)
+        return newData
+    else:   
+        averageRangeFromMap(field,0, map);        
 
-##Get the values
-        originalValues = rangeObject.getFloats(0)
-        newValues = makeFloatArray(len(originalValues), len(originalValues[0]), java.lang.Float.NaN);
-        totals = {};
-        for mapIdx in xrange(len(indices)):
-            indexArray = indices[mapIdx]
-            total = 0;
-            for j in xrange(len(indexArray)):
-                total=total+ originalValues[0][indexArray[j]];
-            totals.update({mapIdx:total})
 
-        for mapIdx in xrange(len(indices)):
-            indexArray = indices[mapIdx]
-            if(len(indexArray)==0):
-                continue;
-            avg = totals[mapIdx]/len(indexArray)
-            for j in xrange(len(indexArray)):            
-                newValues[0][indexArray[j]] = avg
-##Set the samples
-        rangeObject.setSamples(newValues)
-        newData.setSample(timeStep,rangeObject)
-   return newData
+def  averageRangeFromMap(range, timeStep, map):
+    rangeObject = range.clone()
+    indices = gu.findContainedIndices(rangeObject.getDomainSet(), map);
+    originalValues = rangeObject.getFloats(0)
+    newValues = makeFloatArray(len(originalValues), len(originalValues[0]), java.lang.Float.NaN);
+    totals = {};
+    for mapIdx in xrange(len(indices)):
+        indexArray = indices[mapIdx]
+        total = 0;
+        for j in xrange(len(indexArray)):
+            total=total+ originalValues[0][indexArray[j]];
+        totals.update({mapIdx:total})
+
+    for mapIdx in xrange(len(indices)):
+        indexArray = indices[mapIdx]
+        if(len(indexArray)==0):
+            continue;
+        avg = totals[mapIdx]/len(indexArray)
+        for j in xrange(len(indexArray)):            
+            newValues[0][indexArray[j]] = avg
+    rangeObject.setSamples(newValues)
+    return rangeObject;
+
+
 
 
 
