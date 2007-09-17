7a41b7866218b110516f4ff668542b9407e55374
Jeff McWhirter <jeffmc@unavco.org>
2007-09-17 21:36:57 +0000

    snapshot

--- ucar/unidata/idv/ui/JythonShell.java
+++ ucar/unidata/idv/ui/JythonShell.java
@@ -20,24 +20,28 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+
+
 package ucar.unidata.idv.ui;
+
 
 import org.python.core.*;
 import org.python.util.*;
 
-import ucar.unidata.idv.*;
 import ucar.unidata.data.*;
+
+import ucar.unidata.idv.*;
 
 import ucar.unidata.util.GuiUtils;
 import ucar.unidata.util.LogUtil;
 import ucar.unidata.util.Misc;
 import ucar.unidata.util.StringUtil;
 
-
-import java.io.*;
-
 import java.awt.*;
 import java.awt.event.*;
+
+
+import java.io.*;
 
 
 import java.util.ArrayList;
@@ -63,73 +67,146 @@ import javax.swing.tree.*;
  */
 public class JythonShell {
 
+    /** _more_ */
     IntegratedDataViewer idv;
+
+    /** _more_ */
     PythonInterpreter interp;
+
+    /** _more_ */
     JFrame frame;
+
+    /** _more_ */
     JTextField commandFld;
+
+    /** _more_ */
     JEditorPane editorPane;
+
+    /** _more_ */
     StringBuffer sb = new StringBuffer();
+
+    /** _more_ */
     List history = new ArrayList();
+
+    /** _more_ */
     int historyIdx = -1;
 
+
+
+    /**
+     * _more_
+     *
+     * @param theIdv _more_
+     */
     public JythonShell(IntegratedDataViewer theIdv) {
-        this.idv = theIdv;
-        interp = idv.getJythonManager().createInterpreter();
-        interp.set("shell", this);
+        this.idv   = theIdv;
         editorPane = new JEditorPane();
         editorPane.setEditable(false);
         editorPane.setContentType("text/html");
-
-        OutputStream os = new OutputStream() {
-                public  void 	write(int b) {
-                    //                    output(new String(b));
-                }
-                public void 	write(byte[] b, int off, int len) {
-                    output(new String(b,off,len));
-                } 
-            };
-
-        interp.setOut(os);
-        interp.setErr(os);
-
-        JScrollPane scroller = GuiUtils.makeScrollPane(editorPane, 400,300);
-        scroller.setPreferredSize(new Dimension(400,300));
-        commandFld = new  JTextField();
+        createInterpreter();
+        JScrollPane scroller = GuiUtils.makeScrollPane(editorPane, 400, 300);
+        scroller.setPreferredSize(new Dimension(400, 300));
+        commandFld = new JTextField();
         commandFld.addKeyListener(new KeyAdapter() {
-                public void keyPressed(KeyEvent e) {
-                    if (e.getKeyCode() == e.VK_P && e.isControlDown() && history.size()>0) {
-                        if(historyIdx <0 || historyIdx>= history.size())
-                            historyIdx = history.size()-1;
-                        else
-                            historyIdx--;
-                        if(historyIdx>=0 && historyIdx<history.size())
-                            commandFld.setText((String)history.get(historyIdx));
+            public void keyPressed(KeyEvent e) {
+                if ((e.getKeyCode() == e.VK_B) && e.isControlDown()) {
+                    if (commandFld.getCaretPosition() > 0) {
+                        commandFld.setCaretPosition(
+                            commandFld.getCaretPosition() - 1);
                     }
-                    if (e.getKeyCode() == e.VK_N && e.isControlDown() && history.size()>0) {
-                        if(historyIdx <0 || historyIdx>= history.size())
-                            historyIdx = history.size()-1;
-                        else
-                            historyIdx++;
-                        if(historyIdx>=0 && historyIdx<history.size())
-                            commandFld.setText((String)history.get(historyIdx));
-                    }
-
-                }});
-        commandFld.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent ae) {
-                    eval();
                 }
-            });
-        JButton evalBtn = GuiUtils.makeButton("Evaluate:", this,"eval");
-        JComponent bottom = GuiUtils.leftCenter(evalBtn,commandFld);
-        JComponent contents = GuiUtils.centerBottom(scroller,bottom);
-        contents = GuiUtils.inset(contents,5);
-        frame       = new JFrame("Jython Shell");
+                if ((e.getKeyCode() == e.VK_F) && e.isControlDown()) {
+                    if (commandFld.getCaretPosition()
+                            < commandFld.getText().length()) {
+                        commandFld.setCaretPosition(
+                            commandFld.getCaretPosition() + 1);
+                    }
+                }
+                if (((e.getKeyCode() == e.VK_UP)
+                        || ((e.getKeyCode() == e.VK_P)
+                            && e.isControlDown())) && (history.size() > 0)) {
+                    if ((historyIdx < 0) || (historyIdx >= history.size())) {
+                        historyIdx = history.size() - 1;
+                    } else {
+                        historyIdx--;
+                        if (historyIdx < 0) {
+                            historyIdx = 0;
+                        }
+                    }
+                    if ((historyIdx >= 0) && (historyIdx < history.size())) {
+                        commandFld.setText((String) history.get(historyIdx));
+                    }
+                }
+                if (((e.getKeyCode() == e.VK_DOWN)
+                        || ((e.getKeyCode() == e.VK_N)
+                            && e.isControlDown())) && (history.size() > 0)) {
+                    if ((historyIdx < 0) || (historyIdx >= history.size())) {
+                        historyIdx = history.size() - 1;
+                    } else {
+                        historyIdx++;
+                        if (historyIdx >= history.size()) {
+                            historyIdx = history.size() - 1;
+                        }
+                    }
+                    if ((historyIdx >= 0) && (historyIdx < history.size())) {
+                        commandFld.setText((String) history.get(historyIdx));
+                    }
+                }
+
+            }
+        });
+        commandFld.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                eval();
+            }
+        });
+        JButton    evalBtn  = GuiUtils.makeButton("Evaluate:", this, "eval");
+        JComponent bottom   = GuiUtils.leftCenter(evalBtn, commandFld);
+        JComponent contents = GuiUtils.centerBottom(scroller, bottom);
+        contents = GuiUtils.inset(contents, 5);
+
+        JMenuBar menuBar = new JMenuBar();
+        List     items   = new ArrayList();
+        items.add(GuiUtils.makeMenuItem("Clear", this, "clear"));
+        items.add(GuiUtils.makeMenuItem("Export Commands", this,
+                                        "exportHistory"));
+        menuBar.add(GuiUtils.makeMenu("File", items));
+
+
+        items = new ArrayList();
+        List displayMenuItems = new ArrayList();
+
+
+        List cds = idv.getControlDescriptors();
+        Hashtable catMenus = new Hashtable();
+        for (int i = 0; i < cds.size(); i++) {
+            ControlDescriptor cd =  (ControlDescriptor) cds.get(i);
+            JMenu catMenu = (JMenu) catMenus.get(cd.getDisplayCategory());
+            if(catMenu == null) {
+                catMenu = new JMenu(cd.getDisplayCategory());
+                catMenus.put(cd.getDisplayCategory(),catMenu);
+                displayMenuItems.add(catMenu);
+            }
+            catMenu.add(GuiUtils.makeMenuItem(cd.getDescription(),this,"insert", "'"+cd.getControlId()+"'"));
+        }
+
+
+        items.add(GuiUtils.makeMenu("Insert Display Id", displayMenuItems));
+        menuBar.add(GuiUtils.makeMenu("Edit", items));
+
+
+        items = new ArrayList();
+        items.add(GuiUtils.makeMenuItem("Help", this, "showHelp"));
+        menuBar.add(GuiUtils.makeMenu("Help", items));
+
+
+        contents = GuiUtils.topCenter(menuBar, contents);
+        frame    = new JFrame("Jython Shell");
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 idv.getJythonManager().removeInterpreter(interp);
             }
-            });
+        });
         frame.getContentPane().add(contents);
         frame.pack();
         frame.setLocation(100, 100);
@@ -137,19 +214,115 @@ public class JythonShell {
 
     }
 
+
+
+
+    public void insert(String s) {
+        String t = commandFld.getText();
+        int pos = commandFld.getCaretPosition();
+        t = t.substring(0,pos) + s + t.substring(pos);
+        commandFld.setText(t);
+        commandFld.setCaretPosition(pos+s.length());
+    }
+
+
+    /**
+     * _more_
+     */
+    public void showHelp() {
+        idv.getIdvUIManager().showHelp("idv.tools.jythonshell");
+    }
+
+    /**
+     * _more_
+     */
+    public void exportHistory() {
+        if (history.size() == 0) {
+            LogUtil.userMessage("There are no commands to export");
+            return;
+        }
+        String procedureName =
+            GuiUtils.getInput("Enter optional procedure name",
+                              "Procedure name: ", "",
+                              " (Leave blank for no procedure)");
+        if (procedureName == null) {
+            return;
+        }
+        String s;
+        if (procedureName.trim().length() == 0) {
+            s = StringUtil.join("\n", history);
+        } else {
+            s = "def " + procedureName + "():\n" + "    "
+                + StringUtil.join("\n    ", history);
+        }
+        s = "#From shell\n" + s + "\n\n";
+        idv.getJythonManager().appendJython(s);
+    }
+
+
+
+    /**
+     * _more_
+     */
+    private void createInterpreter() {
+        if (interp != null) {
+            idv.getJythonManager().removeInterpreter(interp);
+        }
+        interp = idv.getJythonManager().createInterpreter();
+        interp.set("shell", this);
+        OutputStream os = new OutputStream() {
+            public void write(int b) {
+                //                    output(new String(b));
+            }
+            public void write(byte[] b, int off, int len) {
+                output(new String(b, off, len));
+            }
+        };
+
+        interp.setOut(os);
+        interp.setErr(os);
+    }
+
+
+    /**
+     * _more_
+     */
     public void clear() {
-        sb = new StringBuffer();
-        editorPane.setText(sb.toString());
+        try {
+            historyIdx = -1;
+            history    = new ArrayList();
+            sb         = new StringBuffer();
+            createInterpreter();
+            editorPane.setText("");
+        } catch (Exception exc) {
+            LogUtil.logException(
+                "An error occurred clearing the Jython shell", exc);
+        }
     }
 
+
+
+    /**
+     * _more_
+     */
     public void eval() {
-            String cmd = commandFld.getText();
-            commandFld.setText("");
-            history.add(cmd);
-            historyIdx= -1;
-            eval(cmd);
+        String cmd = commandFld.getText();
+        commandFld.setText("");
+        eval(cmd);
+        /*        if ((historyIdx >= 0) && (historyIdx < history.size())) {
+            if (cmd.equals(history.get(historyIdx))) {
+                return;
+            }
+            }*/
+        history.add(cmd);
+        historyIdx = -1;
     }
 
+    /**
+     * _more_
+     *
+     * @param m _more_
+     */
     private void output(String m) {
         sb.append("<br>\n");
         sb.append(m);
@@ -157,18 +330,28 @@ public class JythonShell {
         editorPane.repaint();
     }
 
+    /**
+     * _more_
+     *
+     * @param jython _more_
+     */
     private void eval(String jython) {
         Misc.run(this, "evalInThread", jython);
     }
 
+    /**
+     * _more_
+     *
+     * @param jython _more_
+     */
     public void evalInThread(String jython) {
         try {
-            output("&gt;<i>" +jython+"</i>");
+            output("&gt;<i>" + jython + "</i>");
             interp.exec(jython);
         } catch (PySyntaxError pse) {
-            output("Syntax error:<br>"+pse);
+            output("Syntax error:<br>" + pse);
         } catch (Exception exc) {
-            output("An error occurred:<br>"+exc);
+            output("An error occurred:<br>" + exc);
         }
     }
 
