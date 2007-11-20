ee4c4a43c0ed7ab0d2f759b3a33751f5e18c8415
Jeff McWhirter <jeffmc@unavco.org>
2007-11-20 16:01:57 +0000

    snapshot

--- ucar/unidata/data/SqlUtils.java
+++ ucar/unidata/data/SqlUtils.java
@@ -25,13 +25,42 @@ package ucar.unidata.data;
 
 
 
+import java.io.File;
 import java.sql.*;
+import ucar.unidata.util.IOUtil;
+import ucar.unidata.util.DateUtil;
 import ucar.unidata.util.Misc;
 
+import java.text.SimpleDateFormat;
 
 public class SqlUtils {
 
     
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static String quote(Object s) {
+        return "\"" + s.toString()  +"\"";
+    }
+
+
+    public static String format(Date d) {
+        return sdf.format(d);
+    }
+
+    public static String comma(Object s1, Object s2) {
+        return s1.toString() +","+s2.toString() ;
+    }
+    public static String comma(Object s1, Object s2,Object s3) {
+        return s1.toString() +","+comma(s2,s3);
+    }
+    public static String comma(Object s1, Object s2,Object s3,Object s4) {
+        return s1.toString() +","+comma(s2,s3,s4);
+    }
+
+    public static String makeInsert(String table, String values) {
+        return "INSERT INTO " + table +" VALUES ("+ values +")";
+    }
+
     public static String makeSelect(String what, String where) {
         return "SELECT " + what + " FROM " + where +";";
     }
@@ -103,7 +132,7 @@ public class SqlUtils {
     }
 
 
-    private static  String[] readString(Statement stmt, int column,String name)
+    private static   String[] readString(Statement stmt, int column,String name)
         throws Exception {
         String [] current = new String[100000];
         int cnt = 0;
@@ -194,6 +223,84 @@ public class SqlUtils {
             return stmt.getResultSet();
         }
     }
+
+ 
+    public static void makeTable(Statement stmt) throws Exception {
+        try {
+            stmt.execute("DROP TABLE nids;");
+        } catch(Exception exc) {}
+        stmt.execute("CREATE TABLE nids (file char(200), KEY(file), date date, INDEX(date),  station char(50) , INDEX(station), product char(5) );");
+        File f = new File("/data/ldm/gempak/nexrad/NIDS");
+        File[]files = f.listFiles();
+        long t1 = System.currentTimeMillis();
+        int cnt = 0;
+        for(int xxx=0;xxx<100;xxx++) {
+        for(int i=0;i<files.length;i++) {
+            File stationDir = files[i];
+            String stn = IOUtil.getFileTail(stationDir.toString());
+            File[]subdirs = stationDir.listFiles();
+            for(int subDirIdx=0;subDirIdx<subdirs.length;subDirIdx++) {
+                File subdir = subdirs[subDirIdx];
+                String product = IOUtil.getFileTail(subdir.toString());                
+
+                File[]radarFiles = subdir.listFiles();
+                for(int dataIdx=0;dataIdx<radarFiles.length;dataIdx++) {
+                    String name = radarFiles[dataIdx].toString();
+                    String sql = SqlUtils.makeInsert("nids",
+                                                     SqlUtils.comma(SqlUtils.quote(name+"_"+xxx),
+                                                                    SqlUtils.quote(SqlUtils.format(new Date(radarFiles[dataIdx].lastModified()))),
+                                                                    SqlUtils.quote(stn),SqlUtils.quote(product)));
+                    //                    System.err.println(sql);
+                    cnt++;
+                    stmt.execute(sql);
+                    if(cnt%10000 ==0) System.err.println("# " +cnt);
+                }
+
+            }
+        }
+        }
+        System.err.println("");
+        long t2 = System.currentTimeMillis();
+        System.err.println("cnt:" + cnt +" time:" + (t2-t1));
+
+    }
+
+
+    public static void eval(Statement stmt, String sql) throws Exception {
+        long t1 = System.currentTimeMillis();
+        System.err.println("sql: " + sql);
+        stmt.execute(sql);
+        String[]results = readString(stmt,1);
+        for(int i=0;i<results.length&&i<10;i++) {
+            System.err.print(results[i]+" ");
+            if(i==9)   System.err.print("...");
+        }
+
+        long t2 = System.currentTimeMillis();
+        System.err.println("\ntime:" + (t2-t1) + " results:" + results.length);
+    }
+
+    public static void main(String[]args) throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/test?zeroDateTimeBehavior=convertToNull";
+        Connection connection = DriverManager.getConnection(url, "jeff", "mypassword");
+        Statement stmt = connection.createStatement();
+        //        makeTable(stmt);
+        //        String s1 = "select * from nids";
+        //        String s2 = "select * from nids where station=" + quote("ABC");
+        for(int i=0;i<args.length;i++) {
+            if(args[i].equals("maketable")) {
+                makeTable(stmt);
+            } else {
+                eval(stmt,args[i]);
+            }
+        }
+        //        makeTable(stmt);
+        //        SqlShell sqlShell  = new SqlShell("Test", connection);
+       
+
+    }
+
 
 
 }
