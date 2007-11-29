52597ebdfd08e592f9e347b683c2b4ad70ca3dd9
Jeff McWhirter <jeffmc@unavco.org>
2007-11-29 14:50:39 +0000

    remove debug

--- ucar/unidata/util/IOUtil.java
+++ ucar/unidata/util/IOUtil.java
@@ -1495,7 +1495,6 @@ public class IOUtil {
         File[] children  = dir.listFiles();
         if(children == null) return true;
         for (int i=0;i<children.length;i++) {
-            if(level==0) System.err.println ("DIR:" + children[i]);
             int what = fileViewer.viewFile(children[i]);
             if(what == FileViewer.DO_STOP) 
                 return false;
