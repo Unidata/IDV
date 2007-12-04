76009fa9f3094a8fd455a3ed756cb5afae5c8439
Jeff McWhirter <jeffmc@unavco.org>
2007-12-04 15:15:46 +0000

    remove bad import

--- ucar/unidata/repository/Permission.java
+++ ucar/unidata/repository/Permission.java
@@ -35,7 +35,6 @@ import ucar.unidata.util.Misc;
 
 import ucar.unidata.util.StringBufferCollection;
 import ucar.unidata.util.StringUtil;
-import ucar.unidata.util.TextResult;
 import ucar.unidata.xml.XmlUtil;
 
 import java.io.File;
