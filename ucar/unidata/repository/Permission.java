b318f4fbba91f1a408ca3b7e4096e11e4bf6f3f6
Jeff McWhirter <jeffmc@unavco.org>
2007-12-04 15:14:43 +0000

    New class

---
+++ ucar/unidata/repository/Permission.java
@@ -0,0 +1,109 @@
+/**
+ * $Id: TrackDataSource.java,v 1.90 2007/08/06 17:02:27 jeffmc Exp $
+ *
+ * Copyright 1997-2005 Unidata Program Center/University Corporation for
+ * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
+ * support@unidata.ucar.edu.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+
+
+package ucar.unidata.repository;
+
+
+import ucar.unidata.data.SqlUtil;
+import ucar.unidata.util.DateUtil;
+import ucar.unidata.util.HtmlUtil;
+import ucar.unidata.util.HttpServer;
+import ucar.unidata.util.IOUtil;
+import ucar.unidata.util.LogUtil;
+import ucar.unidata.util.Misc;
+
+import ucar.unidata.util.StringBufferCollection;
+import ucar.unidata.util.StringUtil;
+import ucar.unidata.util.TextResult;
+import ucar.unidata.xml.XmlUtil;
+
+import java.io.File;
+import java.io.InputStream;
+
+import java.lang.reflect.*;
+
+
+
+import java.net.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+
+
+
+import java.util.regex.*;
+
+
+/**
+ * Class SqlUtil _more_
+ *
+ *
+ * @author IDV Development Team
+ * @version $Revision: 1.3 $
+ */
+public class Permission {
+    private boolean mustBeAdmin = true;
+    private List<String> onlyTheseIps;
+
+    public Permission() {
+        this(true, null);
+    }
+    public Permission(boolean mustBeAdmin) {
+        this(mustBeAdmin, null);
+    }
+
+    public Permission(boolean mustBeAdmin, List<String>onlyTheseIps) {
+        this.mustBeAdmin = mustBeAdmin;
+        this.onlyTheseIps = onlyTheseIps;
+    }
+
+
+    public boolean isRequestOk(Request request, Repository repository) {
+        RequestContext context = request.getRequestContext();
+        if(mustBeAdmin) {
+            User user = context.getUser();
+            if(user == null || !user.getAdmin()) return false;
+        }
+        return true;
+    }
+
+
+
+}
+
