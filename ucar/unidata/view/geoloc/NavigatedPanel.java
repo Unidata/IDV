8df9b326b400a6cd095647cf83d762a28f2470d3
Jeff McWhirter <jeffmc@unavco.org>
2007-12-24 18:24:36 +0000

    remove debug

--- ucar/unidata/view/geoloc/NavigatedPanel.java
+++ ucar/unidata/view/geoloc/NavigatedPanel.java
@@ -307,7 +307,6 @@ public class NavigatedPanel extends JPanel implements MouseListener,
 
     public void setEnabled(boolean enabled) {
         super.setEnabled(enabled);
-        System.err.println(getBounds());
         setNewBounds(getBounds(),true);
         repaint();
     }
