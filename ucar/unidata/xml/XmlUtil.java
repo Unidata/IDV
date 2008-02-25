ddd2caed2311fc34d65ae77aacfc402abe98ecf9
Jeff McWhirter <jeffmc@unavco.org>
2008-02-25 23:29:09 +0000

    Move some code out of StringUtil

--- ucar/unidata/xml/XmlUtil.java
+++ ucar/unidata/xml/XmlUtil.java
@@ -57,8 +57,10 @@ import java.util.List;
 
 
 import javax.xml.parsers.*;
+import java.security.SignatureException;
 
-
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 
 
@@ -1899,6 +1901,49 @@ public abstract class XmlUtil {
             System.exit(0);
         }
     }
+
+
+
+    /** sha algorithm to use */
+    private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+
+    /**
+     * Computes RFC 2104-compliant HMAC signature.
+     *
+     * @param data
+     *     The data to be signed.
+     * @param key
+     *     The signing key.
+     * @return
+     *     The base64-encoded RFC 2104-compliant HMAC signature.
+     * @throws java.security.SignatureException when signature generation fails
+     */
+    public static String calculateRFC2104HMAC(String data, String key)
+            throws java.security.SignatureException {
+        String result;
+        try {
+            // get an hmac_sha1 key from the raw key bytes
+            SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(),
+                                           HMAC_SHA1_ALGORITHM);
+
+            // get an hmac_sha1 Mac instance and initialize with the signing key
+            Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+            mac.init(signingKey);
+
+            // compute the hmac on input data bytes
+            byte[] rawHmac = mac.doFinal(data.getBytes());
+
+            // base64-encode the hmac
+            result = XmlUtil.encodeBase64(rawHmac);
+        } catch (Exception e) {
+            throw new SignatureException("Failed to generate HMAC : "
+                                         + e.getMessage());
+        }
+        return result;
+    }
+
+
+
 
 
 }
