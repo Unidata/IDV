005e229b6f1e50726df9ff6176ee6a2b1db962d0
Jeff McWhirter <jeffmc@unavco.org>
2008-08-27 11:28:26 +0000

    New class to provide a wrapper around the idv for doing servers

---
+++ ucar/unidata/idv/IdvServer.java
@@ -0,0 +1,69 @@
+/*
+ * $Id: IntegratedDataViewer.java,v 1.652 2007/08/22 11:55:41 jeffmc Exp $
+ *
+ * Copyright  1997-2004 Unidata Program Center/University Corporation for
+ * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
+ * support@unidata.ucar.edu.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+
+package ucar.unidata.idv;
+
+
+
+/**
+ * @author IDV development team
+ */
+
+public class IdvServer  {
+    private Object MUTEX = new Object();
+    private  MyIdv idv;
+    private int callCnt=0;
+
+    public IdvServer() throws Exception {
+        idv =new MyIdv();
+    }
+
+
+    public void evaluateIsl(StringBuffer isl) throws Exception {
+        synchronized (MUTEX) {
+            //Make a new one every 100 calls
+            if (callCnt++ > 100) {
+                idv.cleanup();
+                idv =new MyIdv();
+                callCnt = 0;
+            }
+            idv.getImageGenerator().processScriptFile("xml:" + isl);
+            idv.cleanup();
+        }
+
+    }
+
+    public MyIdv getIdv() {
+        return idv;
+    }
+
+    private class MyIdv extends IntegratedDataViewer {
+        public MyIdv() throws Exception {
+            super(false);
+        }
+    }
+
+
+
+}
+
