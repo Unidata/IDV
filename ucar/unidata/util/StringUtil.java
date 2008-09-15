26cd2edb78a9e5d08bbbb85195f5ae2a379f8b9c
Jeff McWhirter <jeffmc@unavco.org>
2008-09-15 11:19:00 +0000

    Add a wildcardToRegexp method

--- ucar/unidata/util/StringUtil.java
+++ ucar/unidata/util/StringUtil.java
@@ -885,6 +885,36 @@ public class StringUtil {
     }
 
 
+   public static String wildcardToRegexp(String wildcard){
+        StringBuffer s = new StringBuffer(wildcard.length());
+        s.append('^');
+        for (int i = 0, is = wildcard.length(); i < is; i++) {
+            char c = wildcard.charAt(i);
+            switch(c) {
+                case '*':
+                    s.append(".*");
+                    break;
+                case '?':
+                    s.append(".");
+                    break;
+                    // escape special regexp-characters
+                case '(': case ')': case '[': case ']': case '$':
+                case '^': case '.': case '{': case '}': case '|':
+                case '\\':
+                    s.append("\\");
+                    s.append(c);
+                    break;
+                default:
+                    s.append(c);
+                    break;
+            }
+        }
+        s.append('$');
+        return(s.toString());
+    }
+
+
+
     /**
      * Check to see if the string starts with a vowel.
      *
