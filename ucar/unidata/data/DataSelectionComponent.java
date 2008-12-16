b609a9f3a23a07a3065a752333a212b41ea6464b
Don Murray <donald.r.murray@gmail.com>
2008-12-16 17:17:18 +0000

    new method

--- ucar/unidata/data/DataSelectionComponent.java
+++ ucar/unidata/data/DataSelectionComponent.java
@@ -1,6 +1,4 @@
 /*
- * $Id: GeoSelectionPanel.java,v 1.17 2006/12/27 20:16:49 jeffmc Exp $
- *
  * Copyright 1997-2004 Unidata Program Center/University Corporation for
  * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
  * support@unidata.ucar.edu.
@@ -20,41 +18,16 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+
 package ucar.unidata.data;
 
-
-import ucar.unidata.geoloc.*;
-import ucar.unidata.geoloc.projection.*;
-
-
-
-import ucar.unidata.idv.DisplayConventions;
-
-import ucar.unidata.ui.LatLonWidget;
-
-import ucar.unidata.util.GuiUtils;
-import ucar.unidata.util.HashCodeUtils;
-import ucar.unidata.util.Misc;
-import ucar.unidata.view.geoloc.*;
-import ucar.unidata.view.geoloc.NavigatedMapPanel;
-
-import visad.*;
-
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.*;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.*;
-import javax.swing.event.*;
+import javax.swing.JComponent;
 
 
 /**
- * Holds geo-location information  - lat/lon bounding box, image size, etc.
- * This is used to pass information from a chooser into a datasource.
+ * Used by a display control and field selector to show arbitrary data
+ * selection stuff. This is used to pass information into a control or
+ * data source.
  */
 public abstract class DataSelectionComponent {
 
@@ -64,7 +37,10 @@ public abstract class DataSelectionComponent {
     /** gui content */
     JComponent contents;
 
-    /** We can be used by a display control and this is the control's data selection */
+    /** 
+     * We can be used by a display control and this is the 
+     * control's data selection 
+     */
     protected DataSelection dataSelection;
 
     /**
@@ -89,7 +65,8 @@ public abstract class DataSelectionComponent {
     /**
      * Make if needed and return the gui contents
      *
-     * @param dataSelection If created by a display control this is the control's dataselection
+     * @param dataSelection If created by a display control 
+     *                      this is the control's dataselection
      * @return gui contents
      */
     public JComponent getContents(DataSelection dataSelection) {
@@ -126,8 +103,14 @@ public abstract class DataSelectionComponent {
         return name;
     }
 
-
-
+    /**
+     * Should this show in the control properties, subclasses can override as
+     * needed.
+     * @return true
+     */
+    public boolean getShowInControlProperties() {
+        return true;
+    }
 
 }
 
