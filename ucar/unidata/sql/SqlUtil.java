9477e304db4dfef8807e6a525c2c063232a1915a
Jeff McWhirter <jeffmc@unavco.org>
2009-03-07 20:57:56 +0000

    snapshot

--- ucar/unidata/sql/SqlUtil.java
+++ ucar/unidata/sql/SqlUtil.java
@@ -1415,7 +1415,7 @@ public class SqlUtil {
             System.err.println(sb);
         }
         String query = makeSelect(what, tables, sb.toString(), extra);
-        //        System.err.println(query);
+        System.err.println(query);
         return connection.prepareStatement(query);
     }
 
