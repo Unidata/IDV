1db6a013c858003732218e48d76837159a1350d1
Don Murray <donald.r.murray@gmail.com>
2009-04-16 21:43:26 +0000

    set the index in cache on the GridServiceProvider since that's what's being used now.

--- ucar/unidata/data/DataManager.java
+++ ucar/unidata/data/DataManager.java
@@ -326,6 +326,7 @@ public class DataManager {
         ucar.nc2.util.DiskCache.setCachePolicy(true);
         // have to do this since nj2.2.20
         ucar.nc2.iosp.grib.GribServiceProvider.setIndexAlwaysInCache(true);
+        ucar.nc2.iosp.grid.GridServiceProvider.setIndexAlwaysInCache(true);
 
         visad.util.ThreadManager.setGlobalMaxThreads(dataContext.getIdv().getMaxRenderThreadCount());
 
