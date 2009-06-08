c2ff13614ba972c3bf1346bbcf8e6973334b4369
Jeff McWhirter <jeffmc@unavco.org>
2009-06-08 20:06:56 +0000

    debug snapshot

--- ucar/unidata/util/HttpServer.java
+++ ucar/unidata/util/HttpServer.java
@@ -324,7 +324,7 @@ public class HttpServer {
                     System.err.println("done readLine:");
                     return sb.toString().trim();
                 }
-                System.err.println("readLine:" + ((int)c)+ " " +getSocket());
+                System.err.println("readLine:" + ((int)c)+ " defined:" +(Character.isDefined(c))+" " + getSocket() );
                 sb.append(c);
             }
         }
