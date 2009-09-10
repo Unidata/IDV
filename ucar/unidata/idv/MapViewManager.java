6180cc4f9da5b97a6edbe5867b7f154697b66c3a
Jeff McWhirter <jeffmc@unavco.org>
2009-09-10 13:30:09 +0000

    Set the frontClippingDistance to some small number for the globe display

--- ucar/unidata/idv/MapViewManager.java
+++ ucar/unidata/idv/MapViewManager.java
@@ -315,6 +315,7 @@ public class MapViewManager extends NavigatedViewManager {
                 new GlobeDisplay(getIdv().getArgsManager().getIsOffScreen(),
                                  dimension, null);
             navDisplay = globeDisplay;
+            globeDisplay.getView().setFrontClipDistance(-2000.0);
             setGlobeBackground(globeDisplay);
         } else {
             Trace.call1("MapViewManager.doMakeDisplayMaster projection");
@@ -1664,9 +1665,19 @@ public class MapViewManager extends NavigatedViewManager {
                                    / 100.;
             setGlobeBackground((GlobeDisplay) getMapDisplay());
         }
+        //        NavigatedDisplay navDisplay = getMapDisplay();
+        //        navDisplay.getView().setFrontClipDistance(new Double(frontClipFld.getText()).doubleValue());
+        //        navDisplay.getView().setBackClipDistance(new Double(backClipFld.getText()).doubleValue());
+
+
+
+
         return true;
     }
 
+
+    JTextField frontClipFld;
+    JTextField backClipFld;
 
     /**
      * _more_
@@ -1678,6 +1689,20 @@ public class MapViewManager extends NavigatedViewManager {
         if (globeBackgroundDisplayable == null) {
             return;
         }
+
+        NavigatedDisplay navDisplay = getMapDisplay();
+        frontClipFld = new JTextField(navDisplay.getView().getFrontClipDistance()+"",8);
+        backClipFld = new JTextField(navDisplay.getView().getFrontClipDistance()+"",8);
+        /*
+        tabbedPane.addTab("Clipping",GuiUtils.formLayout(new Component[]{
+            GuiUtils.rLabel("Front Clipping:"),
+            GuiUtils.left(frontClipFld),
+            GuiUtils.rLabel("Back Clipping:"),
+            GuiUtils.left(backClipFld)
+            }));*/
+
+
+
 
         globeBackgroundLevelSlider = new JSlider(-99, 100,
                 (int) (100 * globeBackgroundLevel));
