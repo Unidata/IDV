20f47c685cef8f037fcdd8806bbd00ce74615c44
Yuan Ho <yuanho@ucar.edu>
2009-10-07 21:03:19 +0000

    Misc

--- ucar/unidata/idv/control/TrajectoryFeatureTypeSoundingControl.java
+++ ucar/unidata/idv/control/TrajectoryFeatureTypeSoundingControl.java
@@ -238,7 +238,7 @@ public class TrajectoryFeatureTypeSoundingControl extends AerologicalSoundingCon
             TrajectoryFeatureTypeAdapter          cta   = adapters.get(i);
             List<CosmicTrajectoryFeatureTypeInfo> infos = cta.getTrackInfos();
             CosmicTrajectoryFeatureTypeInfo       cfti  = infos.get(0);
-            Data[] sddata = cfti.getAerologicalDiagramDataNoWind1();  //.getAerologicalDiagramDataNoWind();
+            Data[] sddata = cfti.getAerologicalDiagramDataArrayNoWind();
             dataList.add(sddata);
             NamedLocationTuple s = cfti.getLatLonPoint();
             latLons[i]    = s.getLatLonPoint();
