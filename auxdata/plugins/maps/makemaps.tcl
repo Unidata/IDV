d55701110bfe0a706c7e037b4d340cf93edd37b0
Jeff McWhirter <jeffmc@unavco.org>
2009-11-05 23:20:27 +0000

    new set of africa maps

--- auxdata/plugins/maps/makemaps.tcl
+++ auxdata/plugins/maps/makemaps.tcl
@@ -21,6 +21,9 @@ proc makeMap {file name args} {
 
 }
 
+proc setCategory {cat} {
+    set ::dfltCategory $cat
+}
 
 proc startPlugin {name desc  args} {
     array set A {-category Maps -color 0x00CC00}
@@ -62,9 +65,7 @@ proc endPlugin {} {
 }
 
 
-
-
-
+if {0} {
 
 
 
@@ -147,6 +148,7 @@ makeMap ${gempak}mefbna.ncp "North America Fcst Bnds"
 endPlugin 
 
 
+
 startPlugin gempak_intl  "Pacific and Atlantic Products" -category "Pacific and Atlantic Products"
 makeMap ${gempak}mefbpe.ncp "East Pacific Products"
 makeMap ${gempak}mefbpn.ncp "North Pacific Products"
@@ -158,7 +160,7 @@ makeMap ${gempak}mefba1.ncp "Atlantic SFC Anal Part 1"
 makeMap ${gempak}mefba2.ncp "Atlantic SFC Anal Part 2"
 endPlugin 
 
-if {0} {
+
 makeMap ${gempak}mefbqv.ncp "QPF Verification"
 makeMap ${gempak}hifiwo.ncp "Flight Information Regn"
 makeMap ${gempak}tpcsus.ncp "Convective Sigmet Bounds"
@@ -171,13 +173,11 @@ makeMap ${gempak}tptvna.ncp "TPC Aviation"
 makeMap ${gempak}dcvaac.ncp "DC VAAC Boundary"
 makeMap ${gempak}hpcsfc.ncp "HPC SFC Anl Boundary"
 makeMap ${gempak}hikfwa.ncp "1000 Fathom Line"
-}
-
 
 
 
 startPlugin europemaps "European Maps" -category Europe -color 0x00CC00 
-makeMap files/OUTLEURO Europe 
+makeMap files/OUTLEURO Europe   OUTLEURO
 makeMap files/austria.shp Austria 
 makeMap files/belgium.shp Belgium 
 makeMap files/czech.shp Czech 
@@ -209,4 +209,119 @@ endPlugin
 #makeMap asclakes.cia "Lakes" -color blue -category "TEST"
 #endPlugin 
 
+
+}
+
+
+
+startPlugin africamaps "African Maps" -category {Africa} -color 0x00CC00 
+setCategory "Africa A-E"
+makeMap africa/Africa.zip  {Africa}
+makeMap africa/ALG_outline.zip  {Algeria outline}
+makeMap africa/ALG.zip          {Algeria }
+makeMap africa/ANG-level_1.zip  {Angola level 1}
+makeMap africa/ANG.zip          {Angola }
+makeMap africa/BEN-level_1.zip  {Benin level 1}
+makeMap africa/BEN.zip          {Benin }
+makeMap africa/BOT-level_1.zip  {Botswana level 1}
+makeMap africa/BOT.zip          {Botswana }
+makeMap africa/BUF-level_1.zip  {Burkina Faso level 1}
+makeMap africa/BUF.zip          {Burkina Faso }
+makeMap africa/BUR-level_1.zip  {Burundi level 1}
+makeMap africa/BUR.zip          {Burundi }
+makeMap africa/CAM-level_1.zip  {Cameroon level 1}
+makeMap africa/CAM.zip          {Cameroon }
+makeMap africa/CAP_outline.zip  {Cape Verde outline}
+makeMap africa/CAP.zip          {Cape Verde }
+makeMap africa/CAR-level_1.zip  {Central African Republic level 1}
+makeMap africa/CAR.zip          {Central African Republic }
+makeMap africa/CDI-level_1.zip  {Ivory Coast level 1}
+makeMap africa/CDI.zip          {Ivory Coast }
+makeMap africa/CHA_outline.zip  {Chad outline}
+makeMap africa/CHA.zip          {Chad }
+makeMap africa/CNG-level_1.zip  {Congo level 1}
+makeMap africa/CNG.zip          {Congo }
+makeMap africa/COM_outline.zip  {Comoros outline}
+makeMap africa/COM.zip          {Comoros }
+makeMap africa/DJI-level_1.zip  {Djibouti level 1}
+makeMap africa/DJI.zip          {Djibouti }
+makeMap africa/EGY-level_1.zip  {Egypt level 1}
+makeMap africa/EGY.zip          {Egypt }
+makeMap africa/EQG-level_1.zip  {Equatorial Guinea level 1}
+makeMap africa/EQG.zip          {Equatorial Guinea }
+makeMap africa/ERI-level_1.zip  {Eritrea level 1}
+makeMap africa/ERI.zip          {Eritrea }
+makeMap africa/ETH-level_1.zip  {Ethiopia level 1}
+makeMap africa/ETH.zip          {Ethiopia }
+
+setCategory "Africa G-M"
+makeMap africa/GAB-level_1.zip  {Gabon level 1}
+makeMap africa/GAB.zip          {Gabon }
+makeMap africa/GHA-level_1.zip  {Ghana level 1}
+makeMap africa/GHA.zip          {Ghana }
+makeMap africa/GIN-level_1.zip  {Guinea level 1}
+makeMap africa/GIN.zip          {Guinea }
+makeMap africa/GUB-level_1.zip  {Guinea Bissau level 1}
+makeMap africa/GUB.zip          {Guinea Bissau }
+makeMap africa/KEN-level_1.zip  {Kenya level 1}
+makeMap africa/KEN.zip          {Kenya }
+makeMap africa/LAJ_outline.zip  {Libya outline}
+makeMap africa/LAJ.zip          {Libya }
+makeMap africa/LES-level_1.zip  {Lesotho level 1}
+makeMap africa/LES.zip          {Lesoth }
+makeMap africa/LIB-level_1.zip  {Liberia level 1}
+makeMap africa/LIB.zip          {Liberia }
+makeMap africa/MAA-level_1.zip  {Malawi level 1}
+makeMap africa/MAA.zip          {Malawi }
+makeMap africa/MAD-level_1.zip  {Madagascar level 1}
+makeMap africa/MAD.zip          {Madgascar }
+makeMap africa/MAL-level_1.zip  {Mali level 1}
+makeMap africa/MAL.zip          {Mali }
+makeMap africa/MAU-level_1.zip  {Mauritania level 1}
+makeMap africa/MAU.zip          {Mauritania }
+makeMap africa/MOR-level_1.zip  {Morocco level 1}
+makeMap africa/MOR.zip          {Morocco }
+makeMap africa/MOZ-level_1.zip  {Mozambique level 1}
+makeMap africa/MOZ.zip          {Mozambique }
+
+setCategory "Africa N-Z"
+makeMap africa/NAM-level_1.zip  {Namibia level 1}
+makeMap africa/NAM.zip          {Namibia }
+makeMap africa/NIG-level_1.zip  {Nigeria level 1}
+makeMap africa/NIG.zip          {Nigeria }
+makeMap africa/NIR-level_1.zip  {Niger level 1}
+makeMap africa/NIR.zip          {Niger }
+makeMap africa/RWA-level_1.zip  {Rwanda level 1}
+makeMap africa/RWA.zip          {Rwanda }
+makeMap africa/SEN-level_1.zip  {Senegal level 1}
+makeMap africa/SEN.zip          {Senegal }
+makeMap africa/SIL-level_1.zip  {Sierra Leone level 1}
+makeMap africa/SIL.zip          {Sierra Leone}
+makeMap africa/SOM-level_1.zip  {Somalia level 1}
+makeMap africa/SOM.zip          {Somalia }
+makeMap africa/SOU-level_1.zip  {South Africa level 1}
+makeMap africa/SOU.zip          {South Africa }
+makeMap africa/STP-level_1.zip  {Sao Tome level 1}
+makeMap africa/STP.zip          {Sao Tome }
+makeMap africa/SUD-level_1.zip  {Sudan level 1}
+makeMap africa/SUD.zip          {Sudan }
+makeMap africa/SWA-level_1.zip  {Swaziland level 1}
+makeMap africa/SWA.zip          {Swaziland }
+makeMap africa/TAN-level_1.zip  {Tanzania level 1}
+makeMap africa/TAN.zip          {Tanzania }
+makeMap africa/TOG-level_1.zip  {Togo level 1}
+makeMap africa/TOG.zip          {Togo }
+makeMap africa/TUN-level_1.zip  {Tunisia level 1}
+makeMap africa/TUN.zip          {Tunisia }
+makeMap africa/UGA-level_1.zip  {Uganda level 1}
+makeMap africa/UGA.zip          {Uganda }
+makeMap africa/WES-level_1.zip  {Western Sahara level 1}
+makeMap africa/WES.zip          {Western Sahara }
+makeMap africa/ZAI-level_1.zip  {Zaire level 1}
+makeMap africa/ZAI.zip          {Zaire }
+makeMap africa/ZAM-level_1.zip  {Zambia level 1}
+makeMap africa/ZAM.zip          {Zambia }
+makeMap africa/ZIM-level_1.zip  {Zimbabwe level 1}  
+makeMap africa/ZIM.zip          {Zimbabwe }
+endPlugin
 
