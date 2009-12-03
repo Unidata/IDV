17be4ab3a6190c00c678bbd456a5b119fbf232e7
Jeff McWhirter <jeffmc@unavco.org>
2009-12-03 20:32:27 +0000

    When subsetting or decimating do not use the dbfile if there is one

--- ucar/unidata/data/gis/ShapeFileDataSource.java
+++ ucar/unidata/data/gis/ShapeFileDataSource.java
@@ -1,4 +1,4 @@
-/*
+/**
  * $Id: ShapeFileDataSource.java,v 1.28 2007/04/16 20:34:52 jeffmc Exp $
  *
  * Copyright  1997-2004 Unidata Program Center/University Corporation for
@@ -20,33 +20,37 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+
 package ucar.unidata.data.gis;
 
 
 import ucar.unidata.data.*;
+import ucar.unidata.geoloc.LatLonPointImpl;
 
-import ucar.visad.data.MapSet;
-import ucar.unidata.gis.shapefile.DbaseFile;
+
+import ucar.unidata.geoloc.LatLonRect;
 import ucar.unidata.gis.shapefile.DbaseData;
-
-import ucar.unidata.util.TwoFacedObject;
+import ucar.unidata.gis.shapefile.DbaseFile;
 import ucar.unidata.util.CacheManager;
 import ucar.unidata.util.GuiUtils;
 import ucar.unidata.util.IOUtil;
 import ucar.unidata.util.JobManager;
 import ucar.unidata.util.Misc;
 
-import ucar.visad.ShapefileAdapter;
+import ucar.unidata.util.TwoFacedObject;
+
 import ucar.visad.MapFamily;
 
+import ucar.visad.ShapefileAdapter;
 
-import ucar.unidata.geoloc.LatLonRect;
-import ucar.unidata.geoloc.LatLonPointImpl;
+import ucar.visad.data.MapSet;
+
 import visad.*;
 
-
-import javax.swing.*;
 import java.awt.*;
+
+
+import java.awt.geom.Rectangle2D;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -56,16 +60,16 @@ import java.net.URL;
 
 import java.rmi.RemoteException;
 
-
-import java.awt.geom.Rectangle2D;
-
 import java.util.ArrayList;
-import java.util.Vector;
 
 import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
+
+
+import javax.swing.*;
 
 
 /**
@@ -76,6 +80,7 @@ import java.util.List;
  */
 public class ShapeFileDataSource extends FilesDataSource {
 
+    /** _more_          */
     private static MapFamily mapFamily = new MapFamily("IDV maps");
 
     /** Property id for the dbfile */
@@ -87,10 +92,14 @@ public class ShapeFileDataSource extends FilesDataSource {
     /** The data. We cache this here ourselves */
     private Data shapefileData;
 
+    /** _more_          */
     double coarseness = 0;
+
+    /** _more_          */
     double lastCoarseness = 0;
 
-    private JComboBox 	    coarsenessCbx;
+    /** _more_          */
+    private JComboBox coarsenessCbx;
 
     /**
      * Dummy constructor so this object can get unpersisted.
@@ -115,51 +124,87 @@ public class ShapeFileDataSource extends FilesDataSource {
     }
 
 
+    /**
+     * _more_
+     */
     public void reloadData() {
         shapefileData = null;
-        dbFile=null;
+        dbFile        = null;
         super.reloadData();
     }
 
 
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
     public boolean canDoGeoSelection() {
         return true;
     }
 
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
     protected boolean canDoGeoSelectionStride() {
         return false;
     }
 
 
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
     protected JComponent doMakeGeoSubsetPropertiesComponent() {
-	JComponent comp = super.doMakeGeoSubsetPropertiesComponent();
-	if(coarsenessCbx==null) {
-	    Object selected = null;
-	    Vector items = new Vector();
-	    double[]values = new double[]{0,1,2,3,4,5,6,7,8,9};
-	    String[]names = new String[]{"Full resolution","........",".......","......",".....","....","...","..",".", "Really coarse"};
-	    for(int i=0;i<values.length;i++) {
-		TwoFacedObject tfo = new TwoFacedObject(names[i], new Double(values[i]));
-		if(values[i]==coarseness) selected = tfo;
-		items.add(tfo);
-	    }
-	    coarsenessCbx = new JComboBox(items);
-	    if(selected!=null)
-		coarsenessCbx.setSelectedItem(selected);
-	}
-	return  GuiUtils.topCenter(GuiUtils.vbox(new JLabel("Note: subsetting the map will invalidate any dbfile entries"),
-						 GuiUtils.left(GuiUtils.label("Resolution:", coarsenessCbx))),
-				   comp);
+        JComponent comp = super.doMakeGeoSubsetPropertiesComponent();
+        if (coarsenessCbx == null) {
+            Object   selected = null;
+            Vector   items    = new Vector();
+            double[] values   = new double[] {
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+            };
+            String[] names    = new String[] {
+                "Full resolution", "........", ".......", "......", ".....",
+                "....", "...", "..", ".", "Really coarse"
+            };
+            for (int i = 0; i < values.length; i++) {
+                TwoFacedObject tfo = new TwoFacedObject(names[i],
+                                         new Double(values[i]));
+                if (values[i] == coarseness) {
+                    selected = tfo;
+                }
+                items.add(tfo);
+            }
+            coarsenessCbx = new JComboBox(items);
+            if (selected != null) {
+                coarsenessCbx.setSelectedItem(selected);
+            }
+        }
+        return GuiUtils.topCenter(
+            GuiUtils.inset(
+                GuiUtils.left(GuiUtils.label("Resolution: ", coarsenessCbx)),
+                5), comp);
     }
 
 
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
     public boolean applyProperties() {
-	if(coarsenessCbx!=null) {
-	    TwoFacedObject tfo =  (TwoFacedObject)coarsenessCbx.getSelectedItem();
-	    coarseness = ((Double)tfo.getId()).doubleValue();
-	}
-	if(!super.applyProperties()) return false;
-	return true;
+        if (coarsenessCbx != null) {
+            TwoFacedObject tfo =
+                (TwoFacedObject) coarsenessCbx.getSelectedItem();
+            coarseness = ((Double) tfo.getId()).doubleValue();
+        }
+        if ( !super.applyProperties()) {
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -283,18 +328,28 @@ public class ShapeFileDataSource extends FilesDataSource {
                                         ? bbox.getLatLonRect()
                                         : null);
 
-	Rectangle2D  box = null;
-	if(llr!=null) {
-	    LatLonPointImpl ul = llr.getUpperLeftPoint();
-	    LatLonPointImpl lr = llr.getLowerRightPoint();
-	    box = new Rectangle2D.Double(ul.getLongitude(), ul.getLatitude()-llr.getHeight(), llr.getWidth(), llr.getHeight());
-	    shapefileData = null;
-	}
 
-	if(coarseness!=lastCoarseness) {
-	    shapefileData = null;
-	}
-	lastCoarseness = coarseness;
+        boolean         useDbFile    = true;
+        boolean         amSubsetting = false;
+
+        Rectangle2D     box          = null;
+        if (llr != null) {
+            LatLonPointImpl ul = llr.getUpperLeftPoint();
+            LatLonPointImpl lr = llr.getLowerRightPoint();
+            box = new Rectangle2D.Double(ul.getLongitude(),
+                                         ul.getLatitude() - llr.getHeight(),
+                                         llr.getWidth(), llr.getHeight());
+            shapefileData = null;
+            amSubsetting  = true;
+            useDbFile     = false;
+        }
+
+        if (coarseness != lastCoarseness) {
+            shapefileData = null;
+            amSubsetting  = true;
+            useDbFile     = false;
+        }
+        lastCoarseness = coarseness;
 
 
         String filename = (String) dataChoice.getId();
@@ -302,20 +357,21 @@ public class ShapeFileDataSource extends FilesDataSource {
         try {
             if (shapefileData == null) {
                 //If its not a shp or zip file then try it with the mapFamily
-                if (!IOUtil.hasSuffix(filename,".shp") &&
-                    !IOUtil.hasSuffix(filename,".zip")) {
+                if ( !IOUtil.hasSuffix(filename, ".shp")
+                        && !IOUtil.hasSuffix(filename, ".zip")) {
                     try {
                         URL url = IOUtil.getURL(filename, getClass());
                         shapefileData = (url == null)
-                            ? (SampledSet) mapFamily.open(filename)
-                            : (SampledSet) mapFamily.open(url);
-                        
+                                        ? (SampledSet) mapFamily.open(
+                                            filename)
+                                        : (SampledSet) mapFamily.open(url);
+
                         return shapefileData;
                     } catch (Exception exc) {
                         exc.printStackTrace();
                     }
                 }
-                
+
 
                 if (getProperty(PROP_CACHEABLE, false)) {
                     bytes = CacheManager.getCachedFile("ShapeFileDataSource",
@@ -334,14 +390,16 @@ public class ShapeFileDataSource extends FilesDataSource {
                     return null;
                 }
 
-		InputStream inputStream  = new ByteArrayInputStream(bytes, 0, bytes.length);
-                ShapefileAdapter sfa =
-                    new ShapefileAdapter(inputStream, filename, box,coarseness);
+                InputStream inputStream = new ByteArrayInputStream(bytes, 0,
+                                              bytes.length);
+                ShapefileAdapter sfa = new ShapefileAdapter(inputStream,
+                                           filename, box, coarseness);
 
 
                 dbFile = sfa.getDbFile();
                 //If this is a .shp file then try to read in the dbf file
-                if ((dbFile == null) && IOUtil.hasSuffix(filename,".shp")) {
+                if (useDbFile && (dbFile == null)
+                        && IOUtil.hasSuffix(filename, ".shp")) {
                     InputStream dbfInputStream = null;
                     try {
                         String dbFilename = IOUtil.stripExtension(filename)
@@ -356,13 +414,22 @@ public class ShapeFileDataSource extends FilesDataSource {
                     }
                 }
                 shapefileData = sfa.getData();
-                setProperties(shapefileData, dbFile);
+                if (useDbFile) {
+                    setProperties(shapefileData, dbFile);
+                }
             }
 
-            if ((requestProperties != null) && (dbFile != null)) {
+            if (useDbFile && (requestProperties != null)
+                    && (dbFile != null)) {
                 requestProperties.put(PROP_DBFILE, dbFile);
             }
-            return shapefileData;
+            Data result = shapefileData;
+
+            if (amSubsetting) {
+                shapefileData = null;
+            }
+
+            return result;
         } catch (Exception exc) {
             logException("Reading shapefile: " + filename, exc, bytes);
         }
@@ -371,43 +438,59 @@ public class ShapeFileDataSource extends FilesDataSource {
 
 
 
-    public static Data readMap(String filename) 
-            throws Exception {
-	if (!IOUtil.hasSuffix(filename,".shp") &&
-	    !IOUtil.hasSuffix(filename,".zip")) {
-	    try {
-	    URL url = IOUtil.getURL(filename, ShapeFileDataSource.class);
-	    Data data =   (url == null)
-		    ? (SampledSet) mapFamily.open(filename)
-		    : (SampledSet) mapFamily.open(url);
-	    return data;
-	    } catch(Exception ignore){}
-	}                
+    /**
+     * _more_
+     *
+     * @param filename _more_
+     *
+     * @return _more_
+     *
+     * @throws Exception _more_
+     */
+    public static Data readMap(String filename) throws Exception {
+        if ( !IOUtil.hasSuffix(filename, ".shp")
+                && !IOUtil.hasSuffix(filename, ".zip")) {
+            try {
+                URL  url  = IOUtil.getURL(filename,
+                                          ShapeFileDataSource.class);
+                Data data = (url == null)
+                            ? (SampledSet) mapFamily.open(filename)
+                            : (SampledSet) mapFamily.open(url);
+                return data;
+            } catch (Exception ignore) {}
+        }
 
 
-	byte[] bytes = IOUtil.readBytes(IOUtil.getInputStream(filename,
-						       ShapeFileDataSource.class));
-	if (bytes == null) {
-	    return null;
-	}
-	ShapefileAdapter sfa =
-	    new ShapefileAdapter(new ByteArrayInputStream(bytes, 0,
-							  bytes.length), filename);
+        byte[] bytes = IOUtil.readBytes(IOUtil.getInputStream(filename,
+                           ShapeFileDataSource.class));
+        if (bytes == null) {
+            return null;
+        }
+        ShapefileAdapter sfa =
+            new ShapefileAdapter(new ByteArrayInputStream(bytes, 0,
+                bytes.length), filename);
 
-	Data data =  sfa.getData();
-	return data;
+        Data data = sfa.getData();
+        return data;
     }
 
 
+    /**
+     * _more_
+     *
+     * @param shapefileData _more_
+     * @param dbFile _more_
+     */
     private void setProperties(Data shapefileData, DbaseFile dbFile) {
-        if(dbFile==null || shapefileData == null || !(shapefileData instanceof UnionSet)) {
+        if ((dbFile == null) || (shapefileData == null)
+                || !(shapefileData instanceof UnionSet)) {
             return;
         }
-        SampledSet[] sets       =((UnionSet)shapefileData).getSets();
-        if(sets.length ==0 ||!(sets[0] instanceof MapSet)) {
+        SampledSet[] sets = ((UnionSet) shapefileData).getSets();
+        if ((sets.length == 0) || !(sets[0] instanceof MapSet)) {
             return;
         }
-        int       numFields  = dbFile.getNumFields();
+        int  numFields     = dbFile.getNumFields();
         List propertyNames = new ArrayList();
         for (int fieldIdx = 0; fieldIdx < numFields; fieldIdx++) {
             String fieldName = dbFile.getFieldName(fieldIdx);
@@ -415,22 +498,24 @@ public class ShapeFileDataSource extends FilesDataSource {
         }
 
         //Some of these might be union sets
-        for(int i=0;i<sets.length;i++) {
-            if(sets[i] instanceof MapSet) {
+        for (int i = 0; i < sets.length; i++) {
+            if (sets[i] instanceof MapSet) {
                 MapSet mapSet = (MapSet) sets[i];
                 mapSet.setPropertyNames(propertyNames);
             }
         }
 
         for (int fieldIdx = 0; fieldIdx < numFields; fieldIdx++) {
-            String fieldName = dbFile.getFieldName(fieldIdx);
+            String    fieldName = dbFile.getFieldName(fieldIdx);
             DbaseData dbData    = dbFile.getField(fieldIdx);
-            List      values = dbData.asList();
-            if(values.size()!=sets.length) {
-                throw new IllegalArgumentException ("DBfile size:" + values.size() + " != number of map lines:" + sets.length);
+            List      values    = dbData.asList();
+            if (values.size() != sets.length) {
+                throw new IllegalArgumentException("DBfile size:"
+                        + values.size() + " != number of map lines:"
+                        + sets.length);
             }
-            for(int i=0;i<sets.length;i++) {
-                if(sets[i] instanceof MapSet) {
+            for (int i = 0; i < sets.length; i++) {
+                if (sets[i] instanceof MapSet) {
                     MapSet mapSet = (MapSet) sets[i];
                     mapSet.setProperty(fieldName, values.get(i));
                 }
@@ -464,21 +549,21 @@ public class ShapeFileDataSource extends FilesDataSource {
     }
 
     /**
-       Set the Coarseness property.
-
-       @param value The new value for Coarseness
-    **/
-    public void setCoarseness (double value) {
-	this.coarseness = value;
+     *  Set the Coarseness property.
+     *
+     *  @param value The new value for Coarseness
+     */
+    public void setCoarseness(double value) {
+        this.coarseness = value;
     }
 
     /**
-       Get the Coarseness property.
-
-       @return The Coarseness
-    **/
-    public double getCoarseness () {
-	return this.coarseness;
+     *  Get the Coarseness property.
+     *
+     *  @return The Coarseness
+     */
+    public double getCoarseness() {
+        return this.coarseness;
     }
 
 
