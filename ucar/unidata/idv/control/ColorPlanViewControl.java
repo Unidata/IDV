1404df248f72655d014b4d5b7fb18645069ff2e7
Don Murray <donald.r.murray@gmail.com>
2010-03-09 22:49:15 +0000

    Testing editing in eclipse and checking it in. J&J

--- ucar/unidata/idv/control/ColorPlanViewControl.java
+++ ucar/unidata/idv/control/ColorPlanViewControl.java
@@ -1,20 +1,18 @@
 /*
- * $Id: ColorPlanViewControl.java,v 1.41 2007/08/09 18:38:39 dmurray Exp $
- *
- * Copyright  1997-2004 Unidata Program Center/University Corporation for
+ * Copyright 1997-2010 Unidata Program Center/University Corporation for
  * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
  * support@unidata.ucar.edu.
- *
+ * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or (at
  * your option) any later version.
- *
+ * 
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
  * General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
@@ -23,31 +21,25 @@
 package ucar.unidata.idv.control;
 
 
-import ucar.unidata.data.DataChoice;
-
-import ucar.unidata.idv.DisplayConventions;
 import ucar.unidata.util.GuiUtils;
-import ucar.unidata.util.Misc;
-import ucar.unidata.util.Range;
-
-import ucar.unidata.util.TwoFacedObject;
 
 import ucar.visad.display.DisplayableData;
 import ucar.visad.display.Grid2DDisplayable;
 
-
-import visad.*;
 import visad.VisADException;
 
-import java.awt.*;
-import java.awt.event.*;
 
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import java.rmi.RemoteException;
 
 import java.util.List;
 
-import javax.swing.*;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 
 
 
@@ -156,14 +148,13 @@ public class ColorPlanViewControl extends PlanViewControl {
                 }
             });
             if (polyModeCombo != null) {
-                controlWidgets.add(
-                    new WrapperWidget(
-                        this, GuiUtils.rLabel("Display:"),
-                        GuiUtils.left(
-                                      GuiUtils.hbox(new Component[]{
-                                GuiUtils.rLabel("Shade Colors:"), toggle,
-                                new JLabel("  Mode: "), polyModeCombo,
-                                new JLabel("  Point Size: "), doMakePointSizeWidget()}))));
+                controlWidgets.add(new WrapperWidget(this,
+                        GuiUtils.rLabel("Display:"),
+                        GuiUtils.left(GuiUtils.hbox(new Component[] {
+                    GuiUtils.rLabel("Shade Colors:"), toggle,
+                    new JLabel("  Mode: "), polyModeCombo,
+                    new JLabel("  Point Size: "), doMakePointSizeWidget()
+                }))));
             } else {
                 controlWidgets.add(new WrapperWidget(this,
                         GuiUtils.rLabel("Shade Colors:"),
@@ -237,4 +228,3 @@ public class ColorPlanViewControl extends PlanViewControl {
     }
 
 }
-
