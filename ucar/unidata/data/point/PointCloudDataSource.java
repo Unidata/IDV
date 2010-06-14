3ea7ecd9cd44ae3f43187b0a72f7c9f5dd47f793
Jeff McWhirter <jeffmc@unavco.org>
2010-06-14 18:29:08 +0000

    add in hillshading, bbox selection (not working), etc.:

--- ucar/unidata/data/point/PointCloudDataSource.java
+++ ucar/unidata/data/point/PointCloudDataSource.java
@@ -1,6 +1,7 @@
 /*
- * Copyright 2010 UNAVCO, 6350 Nautilus Drive, Boulder, CO 80301
- * http://www.unavco.org
+ * Copyright 1997-2010 Unidata Program Center/University Corporation for
+ * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
+ * support@unidata.ucar.edu.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -23,6 +24,7 @@ package ucar.unidata.data.point;
 
 import ucar.unidata.data.*;
 import ucar.unidata.geoloc.*;
+import ucar.unidata.geoloc.projection.LatLonProjection;
 
 import ucar.unidata.geoloc.projection.UtmProjection;
 import ucar.unidata.util.GuiUtils;
@@ -36,6 +38,8 @@ import ucar.unidata.util.Trace;
 import ucar.unidata.view.geoloc.MapProjectionDisplay;
 import ucar.unidata.view.geoloc.NavigatedDisplay;
 
+import ucar.visad.MapProjectionProjection;
+
 import ucar.visad.Util;
 import ucar.visad.display.RGBDisplayable;
 
@@ -44,9 +48,6 @@ import ucar.visad.display.VolumeDisplayable;
 import visad.*;
 
 import visad.georef.MapProjection;
-
-
-import visad.georef.TrivialMapProjection;
 
 import java.awt.Component;
 import java.awt.Container;
@@ -58,6 +59,11 @@ import java.awt.geom.Rectangle2D;
 
 
 import java.io.*;
+
+
+
+
+import java.net.URL;
 
 import java.rmi.RemoteException;
 
@@ -81,7 +87,7 @@ import javax.swing.event.*;
 
 public class PointCloudDataSource extends FilesDataSource {
 
-    /** _more_          */
+    /** _more_ */
     public static final float GRID_MISSING = -99999.9f;
 
     /** _more_ */
@@ -121,6 +127,31 @@ public class PointCloudDataSource extends FilesDataSource {
 
     /** _more_ */
     private String fieldName;
+
+    /** _more_          */
+    private int gridWidth = 800;
+
+    /** _more_          */
+    private int gridHeight = 800;
+
+    /** _more_          */
+    private JTextField gridWidthFld;
+
+    /** _more_          */
+    private JTextField gridHeightFld;
+
+    /** _more_          */
+    private float hillShadeAzimuth = 315;
+
+    /** _more_          */
+    private float hillShadeAngle = 45;
+
+    /** _more_          */
+    private JTextField hillShadeAzimuthFld;
+
+    /** _more_          */
+    private JTextField hillShadeAngleFld;
+
 
 
     /** logging category */
@@ -173,6 +204,94 @@ public class PointCloudDataSource extends FilesDataSource {
     }
 
 
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
+    public boolean canDoGeoSelection() {
+        String filePath = getFilePath();
+        return (filePath.indexOf("${bbox}") >= 0)
+               || (filePath.indexOf("${area.north}") >= 0);
+    }
+
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
+    protected boolean canDoGeoSelectionStride() {
+        return false;
+    }
+
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
+    protected ProjectionImpl getSampleDataProjection() {
+        try {
+            String filePath = getFilePath();
+            if (filePath.indexOf("defaultbbox=") < 0) {
+                return null;
+            }
+            String query = new URL(filePath).getQuery();
+
+            for (String pair : StringUtil.split(query, "&", true, true)) {
+                if (pair.startsWith("defaultbbox=")) {
+                    List<String> toks =
+                        StringUtil.split(
+                            pair.substring("defaultbbox=".length()), ",",
+                            true, true);
+                    //west,south,east,north   
+                    if (toks.size() != 4) {
+                        return null;
+                    }
+                    double west  = Double.parseDouble(toks.get(0));
+                    double south = Double.parseDouble(toks.get(1));
+                    double east  = Double.parseDouble(toks.get(2));
+                    double north = Double.parseDouble(toks.get(3));
+                    Rectangle2D.Double rect = new Rectangle2D.Double(west,
+                                                  north, east - west,
+                                                  north - south);
+                    return new LatLonProjection("", new ProjectionRect(rect));
+                }
+            }
+            return null;
+        } catch (Exception exc) {
+            throw new RuntimeException(exc);
+        }
+    }
+
+
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
+    private boolean canSkip() {
+        String filePath = getFilePath();
+        File   f        = new File(filePath);
+        if ( !f.exists()) {
+            if (filePath.indexOf("${skip}") >= 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
+    private boolean serverHandlesSkip() {
+        return canSkip() && !new File(getFilePath()).exists();
+    }
+
 
     /**
      * _more_
@@ -183,29 +302,40 @@ public class PointCloudDataSource extends FilesDataSource {
         super.getPropertiesComponents(comps);
         String filePath = getFilePath();
         File   f        = new File(filePath);
-        if ( !f.exists()) {
-            if (filePath.indexOf("${skip}") < 0) {
-                if (filePath.indexOf("%24%7Bskip%7D") < 0) {
-                    return;
-                }
-            }
-        }
-        skipFld         = new JTextField("" + skip, 5);
-        delimiterFld    = new JTextField(delimiter, 3);
-        colorByIndexFld = new JTextField("" + colorByIndex, 3);
+        skipFld             = new JTextField("" + skip, 5);
+        gridWidthFld        = new JTextField("" + gridWidth, 5);
+        gridHeightFld       = new JTextField("" + gridHeight, 5);
+        hillShadeAzimuthFld = new JTextField("" + hillShadeAzimuth, 5);
+        hillShadeAngleFld   = new JTextField("" + hillShadeAngle, 5);
+        delimiterFld        = new JTextField(delimiter, 3);
+        colorByIndexFld     = new JTextField("" + colorByIndex, 3);
         if (header != null) {
             comps.add(GuiUtils.rLabel("Header:"));
             comps.add(GuiUtils.lLabel(header));
         }
 
-        comps.add(GuiUtils.rLabel("Skip:"));
-        comps.add(GuiUtils.left(GuiUtils.hbox(skipFld,
-                new JLabel("-1 = default"))));
+        if (canSkip()) {
+            comps.add(GuiUtils.rLabel("Skip:"));
+            comps.add(GuiUtils.left(GuiUtils.hbox(skipFld,
+                    new JLabel("-1 = default"))));
+        }
         comps.add(GuiUtils.rLabel("Delimiter:"));
         comps.add(GuiUtils.left(delimiterFld));
         comps.add(GuiUtils.rLabel("Color by index:"));
         comps.add(GuiUtils.left(colorByIndexFld));
         utmInfo.getPropertiesComponents(comps);
+
+        comps.add(GuiUtils.rLabel("Grid Dimensions:"));
+        comps.add(GuiUtils.left(GuiUtils.hbox(gridWidthFld,
+                new JLabel(" X "), gridHeightFld)));
+
+        comps.add(GuiUtils.rLabel("Hill Shading:"));
+        comps.add(GuiUtils.left(GuiUtils.hbox(GuiUtils.label("Azimuth:",
+                hillShadeAzimuthFld), GuiUtils.label("Angle:",
+                    hillShadeAngleFld))));
+
+
+
     }
 
     /**
@@ -222,6 +352,12 @@ public class PointCloudDataSource extends FilesDataSource {
             return true;
         }
         skip = new Integer(skipFld.getText().trim()).intValue();
+        gridWidth = new Integer(gridWidthFld.getText().trim()).intValue();
+        gridHeight = new Integer(gridHeightFld.getText().trim()).intValue();
+        hillShadeAzimuth =
+            Float.parseFloat(hillShadeAzimuthFld.getText().trim());
+        hillShadeAngle = Float.parseFloat(hillShadeAngleFld.getText().trim());
+
         colorByIndex =
             new Integer(colorByIndexFld.getText().trim()).intValue();
 
@@ -256,26 +392,35 @@ public class PointCloudDataSource extends FilesDataSource {
             throws VisADException, RemoteException {
 
         try {
+
             float[][] pts       = null;
             String    filePath  = getFilePath();
             File      f         = new File(filePath);
             int       skipToUse = skip;
-            if ( !f.exists()) {
-                if (skip >= 0) {
-                    if ((filePath.indexOf("${skip}") >= 0)
-                            || (filePath.indexOf("%24%7Bskip%7D") >= 0)) {
-                        filePath = filePath.replace("${skip}", "" + skip);
-                        filePath = filePath.replace("%24%7Bskip%7D",
-                                "" + skip);
-                        skipToUse = 0;
-                    }
-                }
-            }
 
+            if (serverHandlesSkip()) {
+                filePath  = filePath.replace("${skip}", "" + skip);
+                skipToUse = 0;
+            }
             BufferedReader reader;
             if (f.exists()) {
                 reader = new BufferedReader(new FileReader(f));
             } else {
+
+                if (dataSelection.getGeoSelection() != null) {
+                    GeoLocationInfo gli =
+                        dataSelection.getGeoSelection().getBoundingBox();
+                    if (gli != null) {
+                        //west,south,east,north   
+                        String bbox = gli.getMinLon() + "," + gli.getMinLat()
+                                      + "," + gli.getMaxLon() + ","
+                                      + gli.getMaxLat();
+                        filePath = filePath.replace("${bbox}", bbox);
+                    }
+                }
+
+                System.err.println(filePath);
+
                 InputStream inputStream = IOUtil.getInputStream(filePath,
                                               getClass());
                 //                inputStream = new BufferedInputStream(inputStream, 10000);
@@ -415,7 +560,11 @@ public class PointCloudDataSource extends FilesDataSource {
                                 RealType.Longitude, RealType.Latitude, rt);
 
             if (dataChoice.getId().equals("altitudegrid")) {
-                return makeGrid(pts, rt);
+                return makeGrid(pts, rt, false);
+            }
+
+            if (dataChoice.getId().equals("hillshadegrid")) {
+                return makeGrid(pts, rt, true);
             }
             return makeField(type, pts);
         } catch (Exception exc) {
@@ -427,19 +576,122 @@ public class PointCloudDataSource extends FilesDataSource {
     /**
      * _more_
      *
+     * @param grid _more_
+     * @param azimuth _more_
+     * @param angle _more_
+     *
+     * @return _more_
+     */
+    private float[][] doHillShade(float[][] grid, float azimuth,
+                                  float angle) {
+        float     z                = 1.0f;
+        float     nsres            = 1.0f;
+        float     scale            = 1.0f;
+        float     ewres            = 1.0f;
+
+        float     degreesToRadians = (float) (Math.PI / 180.0);
+        float     radiansToDegrees = (float) (180.0 / Math.PI);
+        int       nYSize           = grid.length;
+        int       nXSize           = grid[0].length;
+        float[][] angles           = new float[nYSize][nXSize];
+        float[]   win              = new float[9];
+        Misc.fillArray(angles, Float.NaN);
+        int i, j;
+        /*  0 1 2
+         *  3 4 5
+         *  6 7 8
+         */
+        for (i = 0; i < nYSize; i++) {
+            for (j = 0; j < nXSize; j++) {
+                if ((i == 0) || (j == 0) || (i == nYSize - 1)
+                        || (j == nXSize - 1)) {
+                    continue;
+                }
+                boolean containsNull = false;
+                win[0] = grid[i - 1][j - 1];
+                win[1] = grid[i - 1][j];
+                win[2] = grid[i - 1][j + 1];
+                win[3] = grid[i][j - 1];
+                win[4] = grid[i][j];
+                win[5] = grid[i][j + 1];
+                win[6] = grid[i + 1][j - 1];
+                win[7] = grid[i + 1][j];
+                win[8] = grid[i + 1][j + 1];
+
+
+                for (int n = 0; n <= 8; n++) {
+                    if ((win[n] != win[n]) || (win[n] == GRID_MISSING)) {
+                        containsNull = true;
+                        break;
+                    }
+                }
+                if (containsNull) {
+                    continue;
+                }
+                // First Slope ...
+
+                float x = (float) (((z * win[0] + z * win[3] + z * win[3]
+                                     + z * win[6]) - (z * win[2] + z * win[5]
+                                         + z * win[5] + z * win[8])) / (8.0
+                                             * ewres * scale));
+
+                float y = (float) (((z * win[6] + z * win[7] + z * win[7]
+                                     + z * win[8]) - (z * win[0] + z * win[1]
+                                         + z * win[1] + z * win[2])) / (8.0
+                                             * nsres * scale));
+
+                float key = (float) Math.sqrt(x * x + y * y);
+                float slope = (float) (90.0
+                                       - Math.atan(key) * radiansToDegrees);
+                float slopePct = 100 * key;
+                float value    = slopePct;
+
+                // ... then aspect...
+                float aspect = (float) Math.atan2(x, y);
+
+                // ... then the shade value
+                float cang =
+                    (float) (Math.sin(angle * degreesToRadians)
+                             * Math.sin(slope * degreesToRadians) + Math.cos(
+                                 angle * degreesToRadians) * Math.cos(
+                                 slope * degreesToRadians) * Math.cos(
+                                 (azimuth - 90.0) * degreesToRadians
+                                 - aspect));
+
+                if (cang <= 0.0) {
+                    cang = 1.0f;
+                } else {
+                    cang = 1.0f + (254.0f * cang);
+                }
+
+                value        = cang;
+
+                angles[i][j] = value;
+            }
+        }
+        return angles;
+    }
+
+
+
+    /**
+     * _more_
+     *
      * @param pts _more_
      * @param type _more_
+     * @param hillshade _more_
      *
      * @return _more_
      *
      * @throws Exception _more_
      */
-    private FieldImpl makeGrid(float[][] pts, RealType type)
+    private FieldImpl makeGrid(float[][] pts, RealType type,
+                               boolean hillshade)
             throws Exception {
 
         boolean   fillMissing = true;
-        int       numCols     = 800;
-        int       numRows     = 800;
+        int       numCols     = gridWidth;
+        int       numRows     = gridHeight;
 
         float     west        = Float.POSITIVE_INFINITY;
         float     south       = Float.POSITIVE_INFINITY;
@@ -561,6 +813,12 @@ public class PointCloudDataSource extends FilesDataSource {
         }
 
 
+        if (hillshade) {
+            type = Util.makeRealType("hillshade" + (typeCnt++), null);
+            latLonGrid = doHillShade(latLonGrid, hillShadeAzimuth,
+                                     hillShadeAngle);
+        }
+
         float[][] gridValues = new float[1][numCols * numRows];
 
         int       m          = 0;
@@ -678,6 +936,12 @@ public class PointCloudDataSource extends FilesDataSource {
         addDataChoice(
             new DirectDataChoice(
                 this, "altitudegrid", "altitude", "Altitude Grid",
+                DataCategory.parseCategories("GRID-2D;", false),
+                new Hashtable()));
+
+        addDataChoice(
+            new DirectDataChoice(
+                this, "hillshadegrid", "hillshade", "Hill Shade",
                 DataCategory.parseCategories("GRID-2D;", false),
                 new Hashtable()));
 
@@ -809,6 +1073,80 @@ public class PointCloudDataSource extends FilesDataSource {
      */
     public String getHeader() {
         return this.header;
+    }
+
+
+    /**
+     *  Set the GridWidth property.
+     *
+     *  @param value The new value for GridWidth
+     */
+    public void setGridWidth(int value) {
+        this.gridWidth = value;
+    }
+
+    /**
+     *  Get the GridWidth property.
+     *
+     *  @return The GridWidth
+     */
+    public int getGridWidth() {
+        return this.gridWidth;
+    }
+
+    /**
+     *  Set the GridHeight property.
+     *
+     *  @param value The new value for GridHeight
+     */
+    public void setGridHeight(int value) {
+        this.gridHeight = value;
+    }
+
+    /**
+     *  Get the GridHeight property.
+     *
+     *  @return The GridHeight
+     */
+    public int getGridHeight() {
+        return this.gridHeight;
+    }
+
+
+    /**
+     *  Set the HillShadeAzimuth property.
+     *
+     *  @param value The new value for HillShadeAzimuth
+     */
+    public void setHillShadeAzimuth(float value) {
+        this.hillShadeAzimuth = value;
+    }
+
+    /**
+     *  Get the HillShadeAzimuth property.
+     *
+     *  @return The HillShadeAzimuth
+     */
+    public float getHillShadeAzimuth() {
+        return this.hillShadeAzimuth;
+    }
+
+    /**
+     *  Set the HillShadeAngle property.
+     *
+     *  @param value The new value for HillShadeAngle
+     */
+    public void setHillShadeAngle(float value) {
+        this.hillShadeAngle = value;
+    }
+
+    /**
+     *  Get the HillShadeAngle property.
+     *
+     *  @return The HillShadeAngle
+     */
+    public float getHillShadeAngle() {
+        return this.hillShadeAngle;
     }
 
 
