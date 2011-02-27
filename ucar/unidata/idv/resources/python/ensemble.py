3126abf96676fabe2e525b3222fb0f78564b202d
Yuan Ho <yuanho@ucar.edu>
2011-02-27 02:51:11 +0000

    adding new  file for ensemble function

---
+++ ucar/unidata/idv/resources/python/ensemble.py
@@ -0,0 +1,17 @@
+""" This is the doc for the ensemble grid module """
+
+def ens_savg(grid):
+   """ basic ensemble average """
+   return GridMath.averageOverMembers(grid)
+
+def ens_ssprd(grid):
+   """ basic ensemble average """
+   return GridMath.ensembleStandardDeviation(grid)
+
+def ens_smax(grid):
+   """ max value of all member """
+   return GridMath.ensembleHighestValues(grid)
+
+def ens_smin(grid):
+   """ min value of all member """
+   return GridMath.ensembleLowestValues(grid)
