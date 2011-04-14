f5c4fdc94ff22639f45d9e74936d41a09a8f8d9c
Julien Chastang <julien.c.chastang@gmail.com>
2011-04-14 22:58:19 +0000

    Making sure I am not breaking the old API.

--- test/ucar/unidata/util/IOUtilTest.java
+++ test/ucar/unidata/util/IOUtilTest.java
@@ -13,6 +13,6 @@ public class IOUtilTest {
 		final String FS = System.getProperty("file.separator");
 		assertEquals("Junit error", "foo" + FS + "bar",IOUtil.joinDir("foo", "bar"));
 		assertEquals("Junit error", "foo" + FS + "bar" + FS + "foobar",IOUtil.joinDirs("foo", "bar", "foobar"));
-		assertEquals("Junit error", FS + "foobar",IOUtil.joinDir(new File(""), "foobar"));
+		assertEquals("Junit error",  FS + "foobar",IOUtil.joinDir(new File(""), "foobar"));
 	}
 }
