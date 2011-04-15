d214fd9a88f49bec70c2dc5a607951c8d69d353f
Julien Chastang <julien.c.chastang@gmail.com>
2011-04-15 22:15:54 +0000

    New runIDV batch script for windows. If anyone on windows OS feels like it, they can test this script out and let me know if it works :-)

--- ucar/unidata/idv/release/installer/runIDVNew.bat
+++ ucar/unidata/idv/release/installer/runIDVNew.bat
@@ -1,5 +1,22 @@
 @echo off
 
+REM ##############################################################################
+REM  Script:  runIDV
+REM 
+REM  Purpose: script to launch the IDV
+REM 
+REM  Syntax:  runIDV <idv options>
+REM 
+REM  Notes:   In past versions of the IDV, users had to change this script to
+REM  manipulate memory settings. The IDV now configures the  appropriate memory. 
+REM  Users can also change the memory via the Preferences  menu. In exceptional 
+REM  situations where the IDV may not start due to memory issues, it may be 
+REM  necessary to bootstrap the memory size. In this case, please uncomment the 
+REM  idv_memory section below and subsequently choose memory via the Preferences
+REM  menu.  Be sure to comment it out that after setting the memory via the 
+REM  Preferences if you want the preference to take effect. 
+REM ##############################################################################
+
 setlocal
 
 FOR /F "tokens=*" %%i IN ('jre\bin\java -cp idv.jar
@@ -11,11 +28,15 @@ IF %idv_memory%.==. (
 	    
 REM Stripping quotes
 set idv_memory=%idv_memory:"=%
+
+REM See important note about this above. To bootstrap the IDV memory, uncomment 
+REM the line below and set to a value  in megabytes. 
+REM idv_memory=512
 	    
 jre\bin\java -Xmx%idv_memory%m -Didv.enableStereo=false -jar idv.jar %*
 	    
 REM Use the line below instead if you want to use the D3D version of Java 3D
-REM jre\bin\java -Xmx512m -Dj3d.rend=d3d -jar idv.jar %*
+REM jre\bin\java -Xmx%idv_memory%m -Dj3d.rend=d3d -jar idv.jar %*
 	    
 endlocal
    
