2c83da44c731e25f23d22c8497ce18c62969dab2
Julien Chastang <julien.c.chastang@gmail.com>
2011-04-19 23:22:03 +0000

    Another refactoring to ensure the user does not shoot him/herself in the foot. What did I break this time?

--- ucar/unidata/idv/SystemPreference.java
+++ ucar/unidata/idv/SystemPreference.java
@@ -15,6 +15,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import java.math.BigInteger;
+
 import java.text.MessageFormat;
 
 import java.util.ArrayList;
@@ -30,6 +31,7 @@ import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+
 
 /**
  * Preferences for the "System" tab. Thanks to McV team for providing hints
@@ -49,18 +51,18 @@ public class SystemPreference {
 
     /** Min value for the slider. */
     private static final int MIN_SLIDER_VALUE = 0;
-    
-    /**The tolerance for the slider. */    
-    private final long       TOLERANCE        = DEFAULT_MEMORY / 100;
+
+    /** The tolerance for the slider. */
+    private static final long TOLERANCE = DEFAULT_MEMORY / 100;
 
     /** If system memory is available display slider. */
-    private final boolean displaySlider = SystemMemory.isMemoryAvailable();
+    private static final boolean DISPLAY_SLIDER = SystemMemory.isMemoryAvailable();
 
     /** The jtb bg. */
     private ButtonGroup jtbBg;
 
     /** The memory. */
-    private final AtomicLong memory;
+    private final MyMemory memory;
 
     /** The slider. */
     private JSlider slider;
@@ -96,19 +98,15 @@ public class SystemPreference {
      *            the memory
      */
     SystemPreference(final AtomicLong memory) {
-        if (!withinSliderBounds(convertToPercent(memory.get()))) {
-            memory.set(DEFAULT_MEMORY);
-        }
+        this.memory = new MyMemory(memory);
 
-        this.memory = memory;
-
-        if (displaySlider) {
+        if (DISPLAY_SLIDER) {
             createSlider();
         }
 
         createText();
 
-        if (displaySlider) {
+        if (DISPLAY_SLIDER) {
             containerXable(textSubComp, false);
             containerXable(sliderSubComp, true);
             sliderButton.setSelected(true);
@@ -148,7 +146,7 @@ public class SystemPreference {
         final String postLabelText   = " of available memory (%d/" + SystemMemory.getMemoryInMegabytes() + " megabytes"
                                        + ")";
 
-        sliderLabel = new JLabel(MessageFormat.format(sliderLabelText, convertToPercent(memory.get())));
+        sliderLabel = new JLabel(MessageFormat.format(sliderLabelText, Math.round(convertToPercent(memory.get()))));
 
         final JLabel         postLabel       = new JLabel(String.format(postLabelText, memory.get()));
         final ChangeListener percentListener = new ChangeListener() {
@@ -157,8 +155,7 @@ public class SystemPreference {
                     return;
                 }
 
-                int sliderValue = massageSliderVal(((JSlider)evt.getSource()).getValue());
-
+                int        sliderValue = massageSliderVal(((JSlider) evt.getSource()).getValue());
                 final long n           = convertToNumber(sliderValue);
 
                 // Preventing superfluous changes that will confuse the user.
@@ -166,16 +163,15 @@ public class SystemPreference {
                 // then switches over to the slider. In this situation, the memory will drift
                 // slightly because of the low precision of a percent.
                 if (Math.abs(n - memory.get()) > TOLERANCE) {
-                    memory.getAndSet(n);
+                    memory.set(n);
                 }
 
                 sliderLabel.setText(MessageFormat.format(sliderLabelText, sliderValue));
                 postLabel.setText(String.format(postLabelText, memory.get()));
             }
-
         };
         final JComponent[] sliderComps = GuiUtils.makeSliderPopup(MIN_SLIDER_VALUE, MAX_SLIDER_VALUE,
-                                             convertToPercent(memory.get()), percentListener);
+                                             Math.round(convertToPercent(memory.get())), percentListener);
 
         slider = (JSlider) sliderComps[1];
         slider.setMajorTickSpacing(20);
@@ -189,32 +185,37 @@ public class SystemPreference {
             public void actionPerformed(ActionEvent e) {
                 containerXable(textSubComp, false);
                 containerXable(sliderSubComp, true);
-                slider.setValue(convertToPercent(memory.get()));
+                slider.setValue(Math.round(convertToPercent(memory.get())));
                 textButton.setSelected(false);
             }
         });
         sliderSubComp = GuiUtils.center(GuiUtils.hbox(sliderLabel, sliderComps[0], postLabel));
         sliderComp    = GuiUtils.hbox(sliderButton, sliderSubComp);
     }
-    
-	/**
-	 * If the user chooses slider end points, fix
-	 *
-	 * @param i the i
-	 * 
-	 * @return the fixed value
-	 */
-	private static int massageSliderVal(final int i) {
-		int  sliderValue = i;
-        if (sliderValue == MIN_SLIDER_VALUE) {
-        	sliderValue = 1; 
-        }
-        if (sliderValue == MAX_SLIDER_VALUE) {
-        	sliderValue = 1; 
-        }
-		return sliderValue;
-	}
 
+    /**
+     * If the user chooses slider end points, fix.
+     *
+     * @param p the p
+     * @return the fixed value
+     */
+    private static int massageSliderVal(final int p) {
+        int sliderValue = p;
+
+        if (sliderValue == MIN_SLIDER_VALUE) {
+            sliderValue = 1;
+        }
+
+        if (sliderValue == MAX_SLIDER_VALUE) {
+            sliderValue = 1;
+        }
+
+        if (convertToNumber(p) < SystemMemory.DEFAULT_MEMORY) {
+            sliderValue = Math.round(convertToPercent(SystemMemory.DEFAULT_MEMORY));
+        }
+
+        return sliderValue;
+    }
 
     /**
      * Creates the memory text UI.
@@ -236,16 +237,7 @@ public class SystemPreference {
                 final String t = ((JTextField) e.getSource()).getText();
 
                 if ((t != null) && (t.length() > 0)) {
-                    final long i = new BigInteger(t).longValue();
-                    final float p = ((float)i / SystemMemory.getMemoryInMegabytes()) * 100;
-
-                    if (withinSliderBounds(p)) {
-                        memory.getAndSet(i);
-                    } else if (p < MIN_SLIDER_VALUE) {
-                        memory.getAndSet(convertToNumber(MIN_SLIDER_VALUE));
-                    } else if (p > MAX_SLIDER_VALUE) {
-                        memory.getAndSet(convertToNumber(MAX_SLIDER_VALUE));
-                    }
+                    memory.set(new BigInteger(t).longValue());
                 }
             }
             @Override
@@ -266,7 +258,7 @@ public class SystemPreference {
             }
         });
         textSubComp = GuiUtils.hbox(text, new JLabel(" megabytes"));
-        textComp    = displaySlider
+        textComp    = DISPLAY_SLIDER
                       ? GuiUtils.hbox(textButton, textSubComp)
                       : textSubComp;
     }
@@ -300,27 +292,14 @@ public class SystemPreference {
     }
 
     /**
-     * Check if number is within slider bounds.
-     *
-     * @param number
-     *            the number
-     * @return is the number within slider bounds.
-     */
-    private boolean withinSliderBounds(final float i) {
-        return (!displaySlider)
-               ? true
-               : (i >= MIN_SLIDER_VALUE) && (i <= MAX_SLIDER_VALUE);
-    }
-
-    /**
      * Convert memory to percent.
      *
      * @param number
      *            the number
-     * @return the int
+     * @return the float
      */
-    private static int convertToPercent(final long number) {
-        return Math.round(((float) number / SystemMemory.getMemoryInMegabytes()) * 100);
+    private static float convertToPercent(final long number) {
+        return ((float) number / SystemMemory.getMemoryInMegabytes()) * 100;
     }
 
     /**
@@ -331,6 +310,84 @@ public class SystemPreference {
      * @return the long
      */
     private static long convertToNumber(final int percent) {
-        return  Math.round((percent / 100f) * SystemMemory.getMemoryInMegabytes());
+        return Math.round((percent / 100f) * SystemMemory.getMemoryInMegabytes());
+    }
+
+    /**
+     * A wrapper class to make sure memory value stays within range, hopefully.
+     */
+    private static class MyMemory {
+
+        /** The memory. */
+        private final AtomicLong memory;
+
+        /**
+         *     Instantiates a new my memory.
+         *    
+         *     @param memory the memory
+         */
+        private MyMemory(final AtomicLong memory) {
+            this.memory = memory;
+            memory.getAndSet(massage(memory.get()));
+        }
+
+        /**
+         *     Sets the memory. It massages, if necessary.
+         *    
+         *     @param i the i
+         */
+        private void set(final long i) {
+            memory.getAndSet(massage(i));
+        }
+
+        /**
+         *     Gets the the memory.
+         *    
+         *     @return the long
+         */
+        private long get() {
+            return memory.get();
+        }
+
+        /**
+         * Check if number is within slider bounds.
+         *
+         * @param i the i
+         * @return is the number within slider bounds.
+         */
+        private static boolean withinSliderBounds(final float i) {
+            return (!DISPLAY_SLIDER)
+                   ? true
+                   : (i >= MIN_SLIDER_VALUE) && (i <= MAX_SLIDER_VALUE);
+        }
+
+        /**
+         *     Massage.
+         *    
+         *     @param i the i
+         *     @return the long
+         */
+        private static long massage(final long i) {
+            long        val;
+            final float p   = convertToPercent(i);
+
+            //First check
+            if (withinSliderBounds(i)) {
+            	val = i;
+            } else if (p < MIN_SLIDER_VALUE) {
+                val = convertToNumber(MIN_SLIDER_VALUE);
+            } else if (p > MAX_SLIDER_VALUE) {
+                val = convertToNumber(MAX_SLIDER_VALUE);
+            } else {
+            	val = i;
+            }
+
+            //Second check
+            if (val < SystemMemory.DEFAULT_MEMORY) {
+                val = SystemMemory.DEFAULT_MEMORY;
+            }
+
+            return val;
+        }
     }
 }
