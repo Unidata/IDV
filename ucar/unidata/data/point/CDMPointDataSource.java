9da699784ef59accc00275d1736c18968c9cbe80
Julien Chastang <julien.c.chastang@gmail.com>
2011-05-26 23:44:21 +0000

    Still much more to do, but it is a rough sketch of what is next.

--- ucar/unidata/data/point/CDMPointDataSource.java
+++ ucar/unidata/data/point/CDMPointDataSource.java
@@ -2,10 +2,16 @@ package ucar.unidata.data.point;
 
 //~--- non-JDK imports --------------------------------------------------------
 
+import java.util.Formatter;
+import java.util.Locale;
+
+import ucar.nc2.constants.FeatureType;
+import ucar.nc2.ft.FeatureCollection;
+import ucar.nc2.ft.FeatureDatasetFactoryManager;
+import ucar.nc2.ft.FeatureDatasetPoint;
 import ucar.unidata.data.DataChoice;
 import ucar.unidata.data.DataSelection;
 import ucar.unidata.geoloc.LatLonRect;
-
 import visad.FieldImpl;
 import visad.VisADException;
 
@@ -27,7 +33,15 @@ public class CDMPointDataSource extends PointDataSource {
      * @see ucar.unidata.data.point.PointDataSource#makeObs(ucar.unidata.data.DataChoice, ucar.unidata.data.DataSelection, ucar.unidata.geoloc.LatLonRect)
      */
     @Override
-    protected FieldImpl makeObs(DataChoice dataChoice, DataSelection subset, LatLonRect bbox) throws Exception {
+    protected FieldImpl makeObs(final DataChoice dataChoice, final DataSelection subset, final LatLonRect bbox) throws Exception {
+    	final Formatter formatter = new Formatter(new StringBuffer(), Locale.US);
+    	//Obviously will have to be parameterized
+    	final FeatureDatasetPoint dataset = (FeatureDatasetPoint)FeatureDatasetFactoryManager.open(FeatureType.STATION_PROFILE, "/tmp/Upperair_20110526_0000.nc", null, formatter);
+    	dataset.getPointFeatureCollectionList();
+    	for (FeatureCollection c : dataset.getPointFeatureCollectionList()) {
+			//Eventually, Logic to pull apart feature collection and put into a FieldImpl will be found here.
+		}
+    	
         return null;
     }
 }
