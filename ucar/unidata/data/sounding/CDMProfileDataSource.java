6e3cfdfab04fb5a379de1be886bbd380ac7cab80
Julien Chastang <julien.c.chastang@gmail.com>
2011-05-27 21:32:11 +0000

    There is still much to do but gaining a better picture of how the FeatureDatasetPoint will be deconstructed whatever road we ultimately go down.

--- ucar/unidata/data/sounding/CDMProfileDataSource.java
+++ ucar/unidata/data/sounding/CDMProfileDataSource.java
@@ -2,17 +2,24 @@ package ucar.unidata.data.sounding;
 
 //~--- non-JDK imports --------------------------------------------------------
 
+import java.util.Date;
 import java.util.Formatter;
 import java.util.Locale;
 
+import ucar.ma2.StructureData;
 import ucar.nc2.constants.FeatureType;
 import ucar.nc2.ft.FeatureCollection;
 import ucar.nc2.ft.FeatureDatasetFactoryManager;
 import ucar.nc2.ft.FeatureDatasetPoint;
+import ucar.nc2.ft.PointFeature;
+import ucar.nc2.ft.ProfileFeature;
+import ucar.nc2.ft.StationProfileFeature;
+import ucar.nc2.ft.StationProfileFeatureCollection;
 import ucar.unidata.data.DataChoice;
 import ucar.unidata.data.DataSelection;
 import ucar.unidata.data.point.PointDataSource;
 import ucar.unidata.geoloc.LatLonRect;
+import ucar.unidata.geoloc.Station;
 import visad.FieldImpl;
 import visad.VisADException;
 
@@ -21,7 +28,9 @@ import visad.VisADException;
  */
 public class CDMProfileDataSource extends PointDataSource {
     
-    /**
+    
+
+	/**
      * Instantiates a new cDM point data source.
      *
      * @throws VisADException the vis ad exception
@@ -38,9 +47,21 @@ public class CDMProfileDataSource extends PointDataSource {
     	final Formatter formatter = new Formatter(new StringBuffer(), Locale.US);
     	//Obviously will have to be parameterized
     	final FeatureDatasetPoint dataset = (FeatureDatasetPoint)FeatureDatasetFactoryManager.open(FeatureType.STATION_PROFILE, "/tmp/Upperair_20110526_0000.nc", null, formatter);
-    	dataset.getPointFeatureCollectionList();
-    	for (FeatureCollection c : dataset.getPointFeatureCollectionList()) {
+    	for (FeatureCollection fc : dataset.getPointFeatureCollectionList()) {
 			//Eventually, Logic to pull apart feature collection and put into a FieldImpl will be found here.
+    		final StationProfileFeatureCollection spfc = (StationProfileFeatureCollection)fc;
+    		for (Station station : spfc.getStations()) {
+    			StationProfileFeature stationProfileFeature = spfc.getStationProfileFeature(station);
+    			for (Date date : stationProfileFeature.getTimes()) {
+    				ProfileFeature profileByDate = stationProfileFeature.getProfileByDate(date);
+    				while (profileByDate.hasNext()) {
+    					PointFeature pf = profileByDate.next();
+    					StructureData data = pf.getData();
+    				}
+				}
+    			//Iterate through all times
+			}
+    		
 		}
     	
         return null;
