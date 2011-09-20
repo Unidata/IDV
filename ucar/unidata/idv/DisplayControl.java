2703edc59d8bc79ed9775320a8073815894b4481
Yuan Ho <yuanho@ucar.edu>
2011-09-20 19:20:52 +0000

    change for test release

--- ucar/unidata/idv/DisplayControl.java
+++ ucar/unidata/idv/DisplayControl.java
@@ -55,8 +55,8 @@ import javax.swing.JLabel;
 public interface DisplayControl extends Sharable {
 
     /** For now this allows us to programmatically turn off the time driver functionality */
-    public static final boolean DOTIMEDRIVER = false;
-    //public static final boolean DOTIMEDRIVER = true;
+   // public static final boolean DOTIMEDRIVER = false;
+    public static final boolean DOTIMEDRIVER = true;
 
 
     /**
