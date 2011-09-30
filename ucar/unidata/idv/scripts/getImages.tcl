ccdba4fb5adad7e848fcb9660d5d13c4935e687a
Julien Chastang <julien.c.chastang@gmail.com>
2011-09-30 17:45:15 +0000

    Had to move the tcl scripts to /opt/bin/webcams according to JenO

--- ucar/unidata/idv/scripts/getImages.tcl
+++ ucar/unidata/idv/scripts/getImages.tcl
@@ -38,7 +38,7 @@ proc defineGroup {group args}  {
 
 
 proc defineImage {url name args}  {
-    array  set A {-group {} -desc {} -id {} -dz {} -ll {}}
+    array  set A {-group {} -desc {} -id {} -dz {} -ll {} -heading {0} }
     array set A $args
     if {$A(-group) == ""} {
 	set tokens [split $name >]
@@ -55,12 +55,14 @@ proc defineImage {url name args}  {
 	regsub -all {[\.\s-]+} $A(-id) {} A(-id)
     }
     if {$A(-desc) == ""} {
-	regexp {(http://[^/]+)/} $url match source
+	if {![regexp {(http://[^/]+)/} $url match source]} {
+	    regexp {(ftp://[^/]+)/} $url match source
+	}
 	set A(-desc) "From: $source"
     }
     lappend ::images $A(-id)
     set A(-ll) [string trim $A(-ll)]
-    array set ::image_$A(-id) [list url $url name $name group $A(-group) desc $A(-desc) dz $A(-dz) latlon $A(-ll)]
+    array set ::image_$A(-id) [list url $url name $name group $A(-group) desc $A(-desc) dz $A(-dz) heading $A(-heading) latlon $A(-ll)]
     if {![info exists ::idsInGroup($A(-group))]} {
 	set ::idsInGroup($A(-group)) [list]
 	lappend ::groups $A(-group)
@@ -70,6 +72,26 @@ proc defineImage {url name args}  {
 
 proc getName {id} {
     set ::image_${id}(name)
+}
+
+
+
+proc getLat {id} {
+    set ll [getLatLon $id]
+    if {$ll!=""} {
+	foreach {lat lon} [split $ll ,] break
+	return $lat
+    }
+    return ""
+}
+
+proc getLon {id} {
+    set ll [getLatLon $id]
+    if {$ll!=""} {
+	foreach {lat lon} [split $ll ,] break
+	return $lon
+    }
+    return ""
 }
 
 proc getLatLon {id} {
@@ -91,6 +113,11 @@ proc getDesc {id} {
 proc getDZ {id} {
     set ::image_${id}(dz)    
 }
+
+proc getHeading {id} {
+    set ::image_${id}(heading)    
+}
+
 
 proc cleanId {id} {
     set id [string tolower $id]
@@ -146,32 +173,58 @@ proc fetchImage {id} {
     set url [getUrl $id]
     set dz [getDZ $id]
 
-    logMsg "\tFetching $url"
-    set tok [::http::geturl $url]
-    logMsg "\tdone"
+    logMsg "\tFetching [getName $id] $url"
+    set tok [::http::geturl $url -timeout 30000]
+    set status [::http::status $tok]
+
+    if {$status == "timeout"} {
+	logMsg "\tfetch timed out"
+	return
+    }
     set body [::http::data $tok]
     upvar #0 $tok httpState
     array set meta $httpState(meta)
     if {![info exists meta(Last-Modified)]} {
-	catch {unset httpState}
-	return
+	logMsg "\t\tNo Last-Modified"
+	set date [clock format [clock seconds]]
+##	catch {unset httpState}
+##	return
+    } else {
+	set date $meta(Last-Modified)
     }
-    set date $meta(Last-Modified)
+#    logMsg "\tdone"
+
+
     catch {unset httpState}
     set dttm [clock scan $date]
 
     if {$dz != ""} {
 	set h [clock format $dttm -format "%H"  -gmt true]
 	regsub {^0} $h {} h
-##	puts "[getName $id] $h"
 	set localTime [expr $h+ $dz]
-##	puts "local time: $localTime" 
-	if {$localTime>18 || $localTime < 6} {
-##	    puts "Excluding: $id"
+	if {$localTime<0} {
+	    set localTime [expr 24+$localTime]
+	}
+#	puts "[getName $id] h: $h -- local: $localTime [clock format $dttm -gmt true]" 
+	if {$localTime>21 || $localTime < 5} {
+	    puts "Excluding: $id"
 	    return
 	}
     }
-    set ext [file extension $url]
+
+
+    if {[info exists meta(Content-Type)]} {
+	set ct $meta(Content-Type)
+	if {![regexp {image/(.*)} $ct match type]} {
+	    logMsg "\t\tUnknown  content type: $ct" 
+##	    puts $body
+	    return
+	}
+	set ext ".$type"
+    } else {
+	set ext [file extension $url]
+	regsub {\?.*$} $ext {} ext
+    }
     set dttmString [clock format $dttm -format "%Y%m%d%H%M%Z" -gmt true]
     set filename  [file join $dir image_${dttmString}$ext ]
     if {![file exists $filename]} {
@@ -186,6 +239,9 @@ proc makeIndexForImageSet {id} {
     set group [getGroup $id]
     set format "yyyyMMddHHmmz"
     set imageXml {<?xml version="1.0" encoding="UTF-8"?>}
+    
+    set imageKml ""
+
     set imageHtml "<html><body>\n<h3>$name</h3>\n<ul>\n"
     append imageXml "\n<images base=\"$::imageRoot/[cleanId $id]\" name=\"$name\" group=\"$group\" format=\"$format\" desc=\"[getDesc $id]\">\n"
     set archiveDir [file join $dir archive]
@@ -200,13 +256,16 @@ proc makeIndexForImageSet {id} {
     }
     set fileList [lsort -index 0  -real -decreasing $fileList]
     set cnt 0
+#Purge
     foreach tuple $fileList {
 	incr cnt
 	foreach {dttm dttmString file} $tuple break
 	set f [file tail $file]
-	if {$cnt>100} {
+	if {$cnt>200} {
 ##	    puts "Renaming $file to $archiveDir"
-	    file rename -force $file $archiveDir
+##	    file rename -force $file $archiveDir
+#	    puts "Deleting old $file"
+	    file delete -force $file 
 	    continue;
 	}
 
@@ -221,10 +280,46 @@ proc makeIndexForImageSet {id} {
 		}
 	    }
 	}
+
+
+	set lat [getLat $id]
+	set lon [getLon $id]
+	
+	set imgPath "[cleanId $id]/$f"
+	if {$lat!=""}  {
+	    if {$cnt == 1} {
+		set kml $::kmlTemplate
+		regsub -all {%name%} $kml "Latest Image" kml
+		regsub -all {%lon%} $kml $lon kml
+		regsub -all {%lat%} $kml $lat kml
+		regsub -all {%url%} $kml "$::imageRoot/$imgPath" kml
+		regsub -all {%extra%} $kml "" latest
+		append ::latestKml  "<Folder>\n<name>$name</name>\n<visibility>1</visibility>\n"
+		append ::latestKml $latest
+		append ::latestKml "<NetworkLink><name>Time loop</name><visibility>0</visibility><flyToView>1</flyToView>\n<Link><href>$::imageRoot/[cleanId $id]/index.kml</href></Link></NetworkLink>\n"
+		append ::latestKml  "</Folder>\n";
+	    } 
+	    if {$cnt<20} {
+		regexp {(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)} $dttm match yyyy  mm dd hh min  
+		set formattedTime  "$yyyy-$mm-${dd}T$hh:${min}:00" 
+		set timeStamp "<TimeStamp><when>$formattedTime</when></TimeStamp>"
+		set kml $::kmlTemplate
+		set heading  [getHeading $id]
+		regsub -all {%heading%} $kml "$heading" kml
+		regsub -all {%name%} $kml "$formattedTime" kml
+		regsub -all {%lon%} $kml $lon kml
+		regsub -all {%lat%} $kml $lat kml
+		regsub -all {%url%} $kml "$::imageRoot/$imgPath" kml
+		regsub -all {%extra%} $kml "$timeStamp" latest
+		append imageKml $latest
+	    }
+	} 
+
+
+
 	append imageXml "<image time=\"$dttmString\" file=\"$f\"/>\n"
 	append imageHtml "<li> <a href=\"$f\">$dttmString</a>\n"
 	if {$cnt == 1} {
-	    set imgPath "[cleanId $id]/$f"
 	    catch {
 #	    set thumb [file join [file dirname $file] thumb_[file tail $file]]
 #		set thumbDim 140
@@ -237,6 +332,8 @@ proc makeIndexForImageSet {id} {
     }
 
     append imageXml "</images>\n"
+##    puts "$name - $dir"
+    writeFile [file join $dir index.kml] [kml $imageKml $name 0]
     writeFile [file join $dir index.xml] $imageXml
     writeFile [file join $dir index.html ] $imageHtml 0
 }
@@ -254,10 +351,34 @@ proc writeFile {f c {binary 1}} {
 
 
 
+proc kml {contents name {vis 1}} {
+    set kml {<?xml version="1.0" encoding="UTF-8"?>}
+    append kml "\n"
+    append kml {<kml xmlns="http://earth.google.com/kml/2.2">}
+    append kml "\n"
+    append kml "<Folder>\n<name>$name</name>\n<visibility>$vis</visibility>\n"
+    append kml $contents
+    append kml "\n"
+    append kml {</Folder>}
+    append kml {</kml>}
+    return $kml
+}
+
+
 proc process {justIndex} {
     set ::images [list]
     catch {file mkdir $::imageDir}
     set ::html {<html><body>Images:<ul>}
+
+    puts "Trying to open $::kmlTemplateFile"
+    puts "file exists = [file exists  $::kmlTemplateFile]"
+    set kmlfp  [open $::kmlTemplateFile r]
+    puts "opened it"
+
+    set ::kmlTemplate [read $kmlfp]
+    close $kmlfp
+
+    set ::latestKml ""
     set ::latestHtml {<html><title>Latest Imagery</title><body>}
 
 
@@ -274,6 +395,9 @@ proc process {justIndex} {
     append ::html {</body></html>}
     writeFile [file join $::imageDir index.html] $::html
     writeFile [file join $::imageDir latest.html] $::latestHtml
+    puts "writing kml: [file join $::imageDir latest.kml]"
+
+    writeFile [file join $::imageDir latest.kml] [kml $::latestKml "Latest Webcam Images" 1]
     append ::xml "</imagesets>"
     writeFile [file join $::imageDir index.xml] $::xml
 }
@@ -288,10 +412,20 @@ proc msg {msg} {
 
 
 
-set ::imageSource /live/georesources/webcams/defineImages.tcl
-set ::logFile     /live/georesources/webcams/log.out
-set ::imageDir /live/georesources/webcams/images
-set ::imagePath /georesources/webcams/images
+set dirRoot /opt/bin/webcams/
+set logRoot /data/logs/
+set imgRoot /content/georesources/webcams/
+#set dirRoot ""
+
+
+set ::imageSource ${dirRoot}defineImages.tcl
+set ::logFile     ${logRoot}getImages.log.out
+set ::imageDir ${imgRoot}images
+set ::kmlTemplateFile     ${imgRoot}photooverlay.kml
+
+set ::imagePath ${imgRoot}images
+
+
 set ::verbose 0
 set ::justIndex 0
 set ::wait 15
