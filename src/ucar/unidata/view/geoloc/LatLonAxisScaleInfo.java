Unidata/IDV

commit 3d1356cbf903c37457f1b093bd2bd25a063a760e

move AxisScaleInfo to ucar.visad.display.  make sure old bundles still work.

src/ucar/unidata/view/geoloc/LatLonAxisScaleInfo.java
@@ -1,0 +1,263 @@
+/*
+ * Copyright 1997-2012 Unidata Program Center/University Corporation for
+ * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
+ * support@unidata.ucar.edu.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package ucar.unidata.view.geoloc;
+
+
+
+
+/**
+ * Class to hold lat/lon specific AxisScaleInfo stuff
+ */
+public class LatLonAxisScaleInfo extends ucar.visad.display.AxisScaleInfo {
+
+    /** Some default coordinate formats */
+    public static String[] COORD_FORMATS = {
+        "DDH", "DD", "DD.d", "DD MM'", "DD:MM", "DD:MM:SS", "DD MM'SS\"",
+        "DD.dH", "DD:MMH", "DD:MM:SSH"
+    };
+
+    /** The coord format. */
+    private String coordFormat;
+
+    /** The base label. */
+    private String baseLabel;
+
+    /** The  increment. */
+    private String increment;
+
+    /** Minor division. */
+    private int minorDivision;
+
+    /** Is axis visible. */
+    private boolean visible;
+
+    /** Use 360 degree angles. */
+    private boolean use360;
+
+    /**
+     * Instantiates a new lat lon scale info.
+     */
+    public LatLonAxisScaleInfo() {}
+
+    /**
+     * Gets the coord format.
+     *
+     * @return the coord format
+     */
+    public String getCoordFormat() {
+        return coordFormat;
+    }
+
+    /**
+     * Sets the coord format.
+     *
+     * @param coordFormat the new coord format
+     */
+    public void setCoordFormat(String coordFormat) {
+        this.coordFormat = coordFormat;
+    }
+
+    /**
+     * Set the format
+     *
+     * @param coordFormat  the format
+     */
+    @Deprecated  //9-5-2012
+    public void setCoordFormat(CoordSys coordFormat) {
+        this.coordFormat = COORD_FORMATS[0];
+    }
+
+
+    /**
+     * Gets the base label.
+     *
+     * @return the base label
+     */
+    public String getBaseLabel() {
+        return baseLabel;
+    }
+
+    /**
+     * Sets the base label.
+     *
+     * @param baseLabel the new base label
+     */
+    public void setBaseLabel(String baseLabel) {
+        this.baseLabel = baseLabel;
+    }
+
+    /**
+     * Gets the increment.
+     *
+     * @return the increment
+     */
+    public String getIncrement() {
+        return increment;
+    }
+
+    /**
+     * Sets the increment.
+     *
+     * @param increment the new increment
+     */
+    public void setIncrement(String increment) {
+        this.increment = increment;
+    }
+
+    /**
+     * Gets the minor division.
+     *
+     * @return the minor division
+     */
+    public int getMinorDivision() {
+        return minorDivision;
+    }
+
+    /**
+     * Sets the minor division.
+     *
+     * @param minorDivision the new minor division
+     */
+    public void setMinorDivision(int minorDivision) {
+        this.minorDivision = minorDivision;
+    }
+
+
+    /**
+     * Checks to see if user wants 360 degree angles
+     *
+     * @return true, if is use360
+     */
+    public boolean isUse360() {
+        return use360;
+    }
+
+
+    /**
+     * The user wants 360 degree angles
+     *
+     * @param use360 the new use360
+     */
+    public void setUse360(boolean use360) {
+        this.use360 = use360;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        final int prime  = 31;
+        int       result = 1;
+        result = prime * result + ((baseLabel == null)
+                                   ? 0
+                                   : baseLabel.hashCode());
+        result = prime * result + ((coordFormat == null)
+                                   ? 0
+                                   : coordFormat.hashCode());
+        result = prime * result + ((getFont() == null)
+                                   ? 0
+                                   : getFont().hashCode());
+        result = prime * result + ((increment == null)
+                                   ? 0
+                                   : increment.hashCode());
+        result = prime * result + ((getLabel() == null)
+                                   ? 0
+                                   : getLabel().hashCode());
+        result = prime * result + minorDivision;
+        result = prime * result + (use360
+                                   ? 1231
+                                   : 1237);
+        result = prime * result + (visible
+                                   ? 1231
+                                   : 1237);
+        return result;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        LatLonAxisScaleInfo other = (LatLonAxisScaleInfo) obj;
+        if (baseLabel == null) {
+            if (other.baseLabel != null) {
+                return false;
+            }
+        } else if ( !baseLabel.equals(other.baseLabel)) {
+            return false;
+        }
+        if (coordFormat == null) {
+            if (other.coordFormat != null) {
+                return false;
+            }
+        } else if ( !coordFormat.equals(other.coordFormat)) {
+            return false;
+        }
+        if (getFont() == null) {
+            if (other.getFont() != null) {
+                return false;
+            }
+        } else if ( !getFont().equals(other.getFont())) {
+            return false;
+        }
+        if (increment == null) {
+            if (other.increment != null) {
+                return false;
+            }
+        } else if ( !increment.equals(other.increment)) {
+            return false;
+        }
+        if (getLabel() == null) {
+            if (other.getLabel() != null) {
+                return false;
+            }
+        } else if ( !getLabel().equals(other.getLabel())) {
+            return false;
+        }
+        if (minorDivision != other.minorDivision) {
+            return false;
+        }
+        if (use360 != other.use360) {
+            return false;
+        }
+        if (visible != other.visible) {
+            return false;
+        }
+        return true;
+    }
+
+
+    @Deprecated  //9-5-2012
+    public enum CoordSys {
+        A, B, C, D, E, F, G, H;
+    }
+}

src/ucar/visad/display/AxisScaleInfo.java
@@ -1,0 +1,154 @@
+/*
+ * Copyright 1997-2012 Unidata Program Center/University Corporation for
+ * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
+ * support@unidata.ucar.edu.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package ucar.visad.display;
+
+
+import java.awt.Font;
+
+
+/**
+ * AxisScaleInfo JavaBean
+ */
+public class AxisScaleInfo {
+
+    /** The axis label. */
+    private String label;
+
+    /** Is axis visible. */
+    private boolean visible;
+
+    /** The axis font. */
+    private Font font;
+
+    /**
+     * Instantiates a new lat lon scale info.
+     */
+    public AxisScaleInfo() {}
+
+
+    /**
+     * Gets the label.
+     *
+     * @return the label
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * Sets the label.
+     *
+     * @param label the new label
+     */
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    /**
+     * Gets the font.
+     *
+     * @return the font
+     */
+    public Font getFont() {
+        return font;
+    }
+
+    /**
+     * Sets the font.
+     *
+     * @param font the new font
+     */
+    public void setFont(Font font) {
+        this.font = font;
+    }
+
+    /**
+     * Checks if is visible.
+     *
+     * @return true, if is visible
+     */
+    public boolean isVisible() {
+        return visible;
+    }
+
+    /**
+     * Sets the visible.
+     *
+     * @param visible the new visible
+     */
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        final int prime  = 31;
+        int       result = 1;
+        result = prime * result + ((label == null)
+                                   ? 0
+                                   : label.hashCode());
+        result = prime * result + (visible
+                                   ? 1231
+                                   : 1237);
+        return result;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        AxisScaleInfo other = (AxisScaleInfo) obj;
+        if (font == null) {
+            if (other.font != null) {
+                return false;
+            }
+        } else if ( !font.equals(other.font)) {
+            return false;
+        }
+        if (label == null) {
+            if (other.label != null) {
+                return false;
+            }
+        } else if ( !label.equals(other.label)) {
+            return false;
+        }
+        if (visible != other.visible) {
+            return false;
+        }
+        return true;
+    }
+
+
+}

src/ucar/unidata/view/geoloc/AxisScaleInfo.java
@@ -21,273 +21,16 @@
 package ucar.unidata.view.geoloc;
 
 
-import java.awt.Font;
-
-
 /**
- * AxisScaleInfo JavaBean
+ * AxisScaleInfo JavaBean.  Only here so old bundles don't break
+ * @deprecated
+ * @see LatLonAxisScaleInfo
  */
-public class AxisScaleInfo {
-
-    /** Some default coordinate formats */
-    public static String[] COORD_FORMATS = {
-        "DDH", "DD", "DD.d", "DD MM'", "DD:MM", "DD:MM:SS", "DD MM'SS\"",
-        "DD.dH", "DD:MMH", "DD:MM:SSH"
-    };
-
-    /** The axis label. */
-    private String label;
-
-    /** The coord format. */
-    private String coordFormat;
-
-    /** The base label. */
-    private String baseLabel;
-
-    /** The  increment. */
-    private String increment;
-
-    /** Minor division. */
-    private int minorDivision;
-
-    /** Is axis visible. */
-    private boolean visible;
-
-    /** The axis font. */
-    private Font font;
-
-    /** Use 360 degree angles. */
-    private boolean use360;
+public class AxisScaleInfo extends LatLonAxisScaleInfo {
 
     /**
      * Instantiates a new lat lon scale info.
      */
     public AxisScaleInfo() {}
 
-
-    /**
-     * Gets the label.
-     *
-     * @return the label
-     */
-    public String getLabel() {
-        return label;
-    }
-
-    /**
-     * Sets the label.
-     *
-     * @param label the new label
-     */
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    /**
-     * Gets the coord format.
-     *
-     * @return the coord format
-     */
-    public String getCoordFormat() {
-        return coordFormat;
-    }
-
-    /**
-     * Sets the coord format.
-     *
-     * @param coordFormat the new coord format
-     */
-    public void setCoordFormat(String coordFormat) {
-        this.coordFormat = coordFormat;
-    }
-
-    @Deprecated //9-5-2012
-    public void setCoordFormat(CoordSys coordFormat) {
-    	this.coordFormat = COORD_FORMATS[0];
-    }
-
-
-    /**
-     * Gets the base label.
-     *
-     * @return the base label
-     */
-    public String getBaseLabel() {
-        return baseLabel;
-    }
-
-    /**
-     * Sets the base label.
-     *
-     * @param baseLabel the new base label
-     */
-    public void setBaseLabel(String baseLabel) {
-        this.baseLabel = baseLabel;
-    }
-
-    /**
-     * Gets the increment.
-     *
-     * @return the increment
-     */
-    public String getIncrement() {
-        return increment;
-    }
-
-    /**
-     * Sets the increment.
-     *
-     * @param increment the new increment
-     */
-    public void setIncrement(String increment) {
-        this.increment = increment;
-    }
-
-    /**
-     * Gets the minor division.
-     *
-     * @return the minor division
-     */
-    public int getMinorDivision() {
-        return minorDivision;
-    }
-
-    /**
-     * Sets the minor division.
-     *
-     * @param minorDivision the new minor division
-     */
-    public void setMinorDivision(int minorDivision) {
-        this.minorDivision = minorDivision;
-    }
-
-    /**
-     * Gets the font.
-     *
-     * @return the font
-     */
-    public Font getFont() {
-        return font;
-    }
-
-    /**
-     * Sets the font.
-     *
-     * @param font the new font
-     */
-    public void setFont(Font font) {
-        this.font = font;
-    }
-
-    /**
-     * Checks if is visible.
-     *
-     * @return true, if is visible
-     */
-    public boolean isVisible() {
-        return visible;
-    }
-
-    /**
-     * Sets the visible.
-     *
-     * @param visible the new visible
-     */
-    public void setVisible(boolean visible) {
-        this.visible = visible;
-    }
-
-
-	/**
-	 * Checks to see if user wants 360 degree angles
-	 *
-	 * @return true, if is use360
-	 */
-	public boolean isUse360() {
-		return use360;
-	}
-
-
-	/**
-	 * The user wants 360 degree angles
-	 *
-	 * @param use360 the new use360
-	 */
-	public void setUse360(boolean use360) {
-		this.use360 = use360;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((baseLabel == null) ? 0 : baseLabel.hashCode());
-		result = prime * result
-				+ ((coordFormat == null) ? 0 : coordFormat.hashCode());
-		result = prime * result + ((font == null) ? 0 : font.hashCode());
-		result = prime * result
-				+ ((increment == null) ? 0 : increment.hashCode());
-		result = prime * result + ((label == null) ? 0 : label.hashCode());
-		result = prime * result + minorDivision;
-		result = prime * result + (use360 ? 1231 : 1237);
-		result = prime * result + (visible ? 1231 : 1237);
-		return result;
-	}
-
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		AxisScaleInfo other = (AxisScaleInfo) obj;
-		if (baseLabel == null) {
-			if (other.baseLabel != null)
-				return false;
-		} else if (!baseLabel.equals(other.baseLabel))
-			return false;
-		if (coordFormat == null) {
-			if (other.coordFormat != null)
-				return false;
-		} else if (!coordFormat.equals(other.coordFormat))
-			return false;
-		if (font == null) {
-			if (other.font != null)
-				return false;
-		} else if (!font.equals(other.font))
-			return false;
-		if (increment == null) {
-			if (other.increment != null)
-				return false;
-		} else if (!increment.equals(other.increment))
-			return false;
-		if (label == null) {
-			if (other.label != null)
-				return false;
-		} else if (!label.equals(other.label))
-			return false;
-		if (minorDivision != other.minorDivision)
-			return false;
-		if (use360 != other.use360)
-			return false;
-		if (visible != other.visible)
-			return false;
-		return true;
-	}
-	
-
-	@Deprecated //9-5-2012
-	public enum CoordSys {
-	    A,B,C,D,E,F,G,H;
-	}
 }

src/ucar/unidata/idv/MapViewManager.java
@@ -49,6 +49,7 @@ import ucar.unidata.util.Trace;
 import ucar.unidata.util.TwoFacedObject;
 import ucar.unidata.view.geoloc.AxisScaleInfo;
 import ucar.unidata.view.geoloc.GlobeDisplay;
+import ucar.unidata.view.geoloc.LatLonAxisScaleInfo;
 import ucar.unidata.view.geoloc.LatLonScalePanel;
 import ucar.unidata.view.geoloc.MapProjectionDisplay;
 import ucar.unidata.view.geoloc.NavigatedDisplay;
@@ -322,10 +323,10 @@ public class MapViewManager extends NavigatedViewManager {
     private LatLonScalePanel latLonScaleWidget;
 
     /** Lat axis scale info for unpersistence */
-    private AxisScaleInfo latAxisScaleInfo;
+    private LatLonAxisScaleInfo latAxisScaleInfo;
 
     /** Lon axis scale info for unpersistence */
-    private AxisScaleInfo lonAxisScaleInfo;
+    private LatLonAxisScaleInfo lonAxisScaleInfo;
 
     /**
      *  Default constructor
@@ -446,7 +447,7 @@ public class MapViewManager extends NavigatedViewManager {
             if (mainProjection == null) {
                 if (initLatLonBounds != null) {
                     doNotSetProjection = true;
-                    mainProjection     =
+                    mainProjection =
                         ucar.visad.Util
                             .makeMapProjection(initLatLonBounds.getY()
                                 - initLatLonBounds
@@ -730,10 +731,10 @@ public class MapViewManager extends NavigatedViewManager {
                         || (keyEvent.getKeyCode() == KeyEvent.VK_J)
                         || (keyEvent.getKeyCode() == KeyEvent.VK_K)
                         || (keyEvent.getKeyCode() == KeyEvent.VK_L))) {
-                double[]      matrix = getProjectionControl().getMatrix();
-                double[]      rot           = new double[3];
-                double[]      scale         = new double[3];
-                double[]      trans         = new double[3];
+                double[] matrix = getProjectionControl().getMatrix();
+                double[] rot   = new double[3];
+                double[] scale = new double[3];
+                double[] trans = new double[3];
                 MouseBehavior mouseBehavior =
                     getNavigatedDisplay().getMouseBehavior();
                 mouseBehavior.instance_unmake_matrix(rot, scale, trans,
@@ -853,7 +854,7 @@ public class MapViewManager extends NavigatedViewManager {
         int cnt = 0;
         while (true) {
             cnt++;
-            Rectangle   sb = navDisplay.getDisplayComponent().getBounds();
+            Rectangle sb = navDisplay.getDisplayComponent().getBounds();
             LatLonPoint ul =
                 getMapDisplay().getEarthLocation(
                     getMapDisplay().getSpatialCoordinatesFromScreen(
@@ -1218,13 +1219,13 @@ public class MapViewManager extends NavigatedViewManager {
             logException("Initializing with MapViewManager", exc);
         }
 
-        AxisScaleInfo latAxisScaleInfo = mvm.getLatAxisScaleInfo();
+        LatLonAxisScaleInfo latAxisScaleInfo = mvm.getLatAxisScaleInfo();
 
         if (latAxisScaleInfo != null) {
             setLatAxisScaleInfo(latAxisScaleInfo);
         }
 
-        AxisScaleInfo lonAxisScaleInfo = mvm.getLonAxisScaleInfo();
+        LatLonAxisScaleInfo lonAxisScaleInfo = mvm.getLonAxisScaleInfo();
 
         if (lonAxisScaleInfo != null) {
             setLonAxisScaleInfo(lonAxisScaleInfo);
@@ -1603,10 +1604,10 @@ public class MapViewManager extends NavigatedViewManager {
                            addressReprojectCbx.isSelected());
 
 
-            float             x      = (float) llp.getLongitude().getValue();
-            float             y      = (float) llp.getLatitude().getValue();
-            float             offset = (float) (1.0 / 60.0f);
-            Rectangle2D.Float rect   = new Rectangle2D.Float(x - offset,
+            float x      = (float) llp.getLongitude().getValue();
+            float y      = (float) llp.getLatitude().getValue();
+            float offset = (float) (1.0 / 60.0f);
+            Rectangle2D.Float rect = new Rectangle2D.Float(x - offset,
                                          y - offset, offset * 2, offset * 2);
             if ( !getUseGlobeDisplay() && addressReprojectCbx.isSelected()) {
                 TrivialMapProjection mp =
@@ -2102,7 +2103,7 @@ public class MapViewManager extends NavigatedViewManager {
     private void addProjectionToHistory(MapProjection projection,
                                         String name) {
         String encodedProjection = getIdv().encodeObject(projection, false);
-        TwoFacedObject tfo       = TwoFacedObject.findId(encodedProjection,
+        TwoFacedObject tfo = TwoFacedObject.findId(encodedProjection,
                                  projectionHistory);
         if (tfo != null) {
             projectionHistory.remove(tfo);
@@ -2424,8 +2425,8 @@ public class MapViewManager extends NavigatedViewManager {
 
 
         globeBackgroundLevelSlider = new ZSlider(globeBackgroundLevel);
-        JComponent   levelComp = globeBackgroundLevelSlider.getContents();
-        JComponent[] bgComps   =
+        JComponent levelComp = globeBackgroundLevelSlider.getContents();
+        JComponent[] bgComps =
             GuiUtils.makeColorSwatchWidget(getGlobeBackgroundColorToUse(),
                                            "Globe Background Color");
 
@@ -2777,7 +2778,8 @@ public class MapViewManager extends NavigatedViewManager {
     public void setCurrentAsProjection() {
         try {
             NavigatedDisplay display      = getMapDisplay();
-            Rectangle        screenBounds = display.getComponent().getBounds();
+            Rectangle        screenBounds =
+                display.getComponent().getBounds();
             LatLonPoint      ulLLP        = null;
             LatLonPoint      lrLLP        = null;
 
@@ -3000,8 +3002,8 @@ public class MapViewManager extends NavigatedViewManager {
 
         Hashtable catMenus = new Hashtable();
         for (int i = 0; i < projections.size(); i++) {
-            ProjectionImpl p     = (ProjectionImpl) projections.get(i);
-            List<String>   names = StringUtil.split(p.getName(), ">", true,
+            ProjectionImpl p = (ProjectionImpl) projections.get(i);
+            List<String> names = StringUtil.split(p.getName(), ">", true,
                                      true);
             JMenu  theMenu  = projectionsMenu;
             String catSoFar = "";
@@ -3051,8 +3053,8 @@ public class MapViewManager extends NavigatedViewManager {
             mi.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     try {
-                        String        encodedProjection = (String) tfo.getId();
-                        MapProjection mp                =
+                        String encodedProjection = (String) tfo.getId();
+                        MapProjection mp =
                             (MapProjection) getIdv().decodeObject(
                                 encodedProjection);
                         setMapProjection(mp, true);
@@ -3722,7 +3724,7 @@ public class MapViewManager extends NavigatedViewManager {
      *
      * @return the lat axis scale info
      */
-    public AxisScaleInfo getLatAxisScaleInfo() {
+    public LatLonAxisScaleInfo getLatAxisScaleInfo() {
         if ( !hasDisplayMaster()) {
             return latAxisScaleInfo;
         }
@@ -3745,8 +3747,25 @@ public class MapViewManager extends NavigatedViewManager {
      *             the remote exception
      * @throws VisADException
      *             the VisAD exception
-     */
+     * @deprecated
     public void setLatAxisScaleInfo(AxisScaleInfo axisScaleInfo)
+            throws RemoteException, VisADException {
+
+        setLatAxisScaleInfo((LatLonAxisScaleInfo) axisScaleInfo);
+    }
+     */
+
+    /**
+     * Sets the lat axis scale info.
+     *
+     * @param axisScaleInfo
+     *            the new lat axis scale info
+     * @throws RemoteException
+     *             the remote exception
+     * @throws VisADException
+     *             the VisAD exception
+     */
+    public void setLatAxisScaleInfo(LatLonAxisScaleInfo axisScaleInfo)
             throws RemoteException, VisADException {
 
         this.latAxisScaleInfo = axisScaleInfo;
@@ -3767,7 +3786,7 @@ public class MapViewManager extends NavigatedViewManager {
      *
      * @return the lon axis scale info
      */
-    public AxisScaleInfo getLonAxisScaleInfo() {
+    public LatLonAxisScaleInfo getLonAxisScaleInfo() {
         if ( !hasDisplayMaster()) {
             return lonAxisScaleInfo;
         }
@@ -3790,8 +3809,24 @@ public class MapViewManager extends NavigatedViewManager {
      *             the remote exception
      * @throws VisADException
      *             the vis ad exception
-     */
+     * @deprecated
     public void setLonAxisScaleInfo(AxisScaleInfo axisScaleInfo)
+            throws RemoteException, VisADException {
+        setLonAxisScaleInfo((LatLonAxisScaleInfo) axisScaleInfo);
+    }
+     */
+
+    /**
+     * Sets the lon axis scale info.
+     *
+     * @param axisScaleInfo
+     *            the new lon axis scale info
+     * @throws RemoteException
+     *             the remote exception
+     * @throws VisADException
+     *             the vis ad exception
+     */
+    public void setLonAxisScaleInfo(LatLonAxisScaleInfo axisScaleInfo)
             throws RemoteException, VisADException {
 
         this.lonAxisScaleInfo = axisScaleInfo;

commit 0e259924ed091cefda41784df4d7f2da14c9b79f

fixing bug introduced in 3d1356c. Got to implement hashcode and equals correctly

src/ucar/unidata/view/geoloc/LatLonAxisScaleInfo.java
@@ -156,22 +156,16 @@ public class LatLonAxisScaleInfo extends ucar.visad.display.AxisScaleInfo {
     @Override
     public int hashCode() {
         final int prime  = 31;
-        int       result = 1;
+        int       result = super.hashCode();
         result = prime * result + ((baseLabel == null)
                                    ? 0
                                    : baseLabel.hashCode());
         result = prime * result + ((coordFormat == null)
                                    ? 0
                                    : coordFormat.hashCode());
-        result = prime * result + ((getFont() == null)
-                                   ? 0
-                                   : getFont().hashCode());
         result = prime * result + ((increment == null)
                                    ? 0
                                    : increment.hashCode());
-        result = prime * result + ((getLabel() == null)
-                                   ? 0
-                                   : getLabel().hashCode());
         result = prime * result + minorDivision;
         result = prime * result + (use360
                                    ? 1231
@@ -182,7 +176,6 @@ public class LatLonAxisScaleInfo extends ucar.visad.display.AxisScaleInfo {
         return result;
     }
 
-
     /**
      * {@inheritDoc}
      */
@@ -191,7 +184,7 @@ public class LatLonAxisScaleInfo extends ucar.visad.display.AxisScaleInfo {
         if (this == obj) {
             return true;
         }
-        if (obj == null) {
+        if ( !super.equals(obj)) {
             return false;
         }
         if (getClass() != obj.getClass()) {
@@ -212,25 +205,11 @@ public class LatLonAxisScaleInfo extends ucar.visad.display.AxisScaleInfo {
         } else if ( !coordFormat.equals(other.coordFormat)) {
             return false;
         }
-        if (getFont() == null) {
-            if (other.getFont() != null) {
-                return false;
-            }
-        } else if ( !getFont().equals(other.getFont())) {
-            return false;
-        }
         if (increment == null) {
             if (other.increment != null) {
                 return false;
             }
         } else if ( !increment.equals(other.increment)) {
-            return false;
-        }
-        if (getLabel() == null) {
-            if (other.getLabel() != null) {
-                return false;
-            }
-        } else if ( !getLabel().equals(other.getLabel())) {
             return false;
         }
         if (minorDivision != other.minorDivision) {
@@ -244,5 +223,4 @@ public class LatLonAxisScaleInfo extends ucar.visad.display.AxisScaleInfo {
         }
         return true;
     }
-
 }

src/ucar/visad/display/AxisScaleInfo.java
@@ -98,6 +98,7 @@ public class AxisScaleInfo {
         this.visible = visible;
     }
 
+
     /**
      * {@inheritDoc}
      */
@@ -105,6 +106,9 @@ public class AxisScaleInfo {
     public int hashCode() {
         final int prime  = 31;
         int       result = 1;
+        result = prime * result + ((font == null)
+                                   ? 0
+                                   : font.hashCode());
         result = prime * result + ((label == null)
                                    ? 0
                                    : label.hashCode());