b821e9c862c0dbe66836f4211aace14b2d8ddc28
Julien Chastang <julien.c.chastang@gmail.com>
2014-03-06 17:23:09 -0700

    Migrating the IDV to Java 7

--- src/ucar/unidata/idv/mac/MacBridge.java
+++ src/ucar/unidata/idv/mac/MacBridge.java
@@ -1,20 +1,18 @@
 /*
- * $Id: StateManager.java,v 1.85 2007/08/17 10:50:39 jeffmc Exp $
- *
- * Copyright  1997-2013 Unidata Program Center/University Corporation for
+ * Copyright 1997-2014 Unidata Program Center/University Corporation for
  * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
  * support@unidata.ucar.edu.
- *
+ * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or (at
  * your option) any later version.
- *
+ * 
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
  * General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
@@ -22,59 +20,101 @@
 
 package ucar.unidata.idv.mac;
 
+
+import com.apple.eawt.Application;
+import com.apple.eawt.ApplicationEvent;
+import com.apple.eawt.ApplicationListener;
+
+import ucar.unidata.idv.IdvManager;
+import ucar.unidata.idv.IntegratedDataViewer;
+import ucar.unidata.ui.ImageUtils;
 import ucar.unidata.util.GuiUtils;
-import ucar.unidata.idv.*;
-import com.apple.eawt.*;
 
-import java.awt.*;
+import java.awt.Image;
 
-public class MacBridge extends IdvManager implements ApplicationListener  {
+import javax.swing.ImageIcon;
+import javax.swing.UIManager;
+
+
+/**
+ * The MacBridge class for OS X specific stuff.
+ */
+public class MacBridge extends IdvManager implements ApplicationListener {
 
     /**
-     * Create this manager
+     * Create this manager.
      *
      * @param idv The IDV
-     *
      */
     public MacBridge(IntegratedDataViewer idv) {
         super(idv);
-	init();
+        init();
     }
 
-
-
+    /**
+     * Init
+     */
     private void init() {
-	Application application = Application.getApplication();
-	application.addApplicationListener(this);
-	Image logo  = 
-	    GuiUtils.getImage(getIdv().getProperty("idv.ui.logo",
-						   "/ucar/unidata/idv/images/logo.gif"),getClass());
+        String icon =
+            getIdv().getProperty("idv.ui.logo",
+                                 "/ucar/unidata/idv/images/logo.gif");
 
-	application.setDockIconImage(logo);
-	application.setEnabledPreferencesMenu(true);
+        Application application = Application.getApplication();
+        application.addApplicationListener(this);
+        Image logo = GuiUtils.getImage(icon, getClass());
+
+        application.setDockIconImage(logo);
+        application.setEnabledPreferencesMenu(true);
     }
 
-    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void handleAbout(ApplicationEvent event) {
-	getIdv().getIdvUIManager().about ();
-	event.setHandled(true);
+        getIdv().getIdvUIManager().about();
+        event.setHandled(true);
     }
-    public void handleOpenApplication(ApplicationEvent event) { }
-    public void handleOpenFile(ApplicationEvent event) { }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handleOpenApplication(ApplicationEvent event) {}
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handleOpenFile(ApplicationEvent event) {}
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void handlePreferences(ApplicationEvent event) {
-	getIdv().showPreferenceManager();
-	event.setHandled(true);
+        getIdv().showPreferenceManager();
+        event.setHandled(true);
     }
-    public void handlePrintFile(ApplicationEvent event) { }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handlePrintFile(ApplicationEvent event) {}
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void handleQuit(ApplicationEvent event) {
-	getIdv().quit();
-	event.setHandled(true);
+        getIdv().quit();
+        event.setHandled(true);
     }
 
-    public void handleReOpenApplication(ApplicationEvent event) { } 
-
-
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handleReOpenApplication(ApplicationEvent event) {}
 }
-
-
