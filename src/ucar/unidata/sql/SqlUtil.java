433c0350a2f94d68238dca53270d404755eed391
Julien Chastang <julien.c.chastang@gmail.com>
2014-03-10 16:16:57 -0600

    copyright 2014

--- src/ucar/unidata/sql/SqlUtil.java
+++ src/ucar/unidata/sql/SqlUtil.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 1997-2014 Unidata Program Center/University Corporation for Atmospheric Research
- * Copyright 2010- Jeff McWhirter
+ * Copyright 2010-2014 Jeff McWhirter
  * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
