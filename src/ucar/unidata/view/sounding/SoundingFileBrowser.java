5966ab8292b18a3223b2c3feae3446915809b578
Julien Chastang <julien.c.chastang@gmail.com>
2014-03-11 16:34:57 -0600

    Adding a .gem file filter.

--- src/ucar/unidata/view/sounding/SoundingFileBrowser.java
+++ src/ucar/unidata/view/sounding/SoundingFileBrowser.java
@@ -80,6 +80,10 @@ public class SoundingFileBrowser {
     public static final PatternFileFilter FILTER_CMA_UA =
         new PatternFileFilter(".*\\.ta$", "CMA Upper Air files (*.ta)");
 
+    /** PatternFileFilter for GEMPAK upper air files */
+    public static final PatternFileFilter FILTER_GEM_UA =
+        new PatternFileFilter(".*\\.gem$", "GEMPAK Upper Air files (*.gem)");
+
     /** property for the sounding adapter */
     private Property soundingAdapterProperty;
 
@@ -150,7 +154,8 @@ public class SoundingFileBrowser {
                 String file =
                     FileManager.getReadFile("Select Upper Air File",
                                             Misc.newList(FILTER_NC,
-                                                FILTER_CMA_UA));
+                                                FILTER_CMA_UA,
+                                                FILTER_GEM_UA));
                 if (file == null) {
                     return;
                 }
