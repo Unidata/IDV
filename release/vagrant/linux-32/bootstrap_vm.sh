552c3848dcac310a8b2339ae011f2e6e5c66a383
Julien Chastang <julien.c.chastang@gmail.com>
2015-07-16 16:41:34 -0600

    Vagrant VMs make creation of JREs much much easier

---
+++ release/vagrant/linux-32/bootstrap_vm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y curl x11-apps libxi6 libxtst6
+
+sudo mkdir /home/vagrant/download
+
+sudo curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie"  http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jre-8u51-linux-i586.tar.gz > /home/vagrant/download/jre-8u51-linux-i586.tar.gz
+
+sudo tar xzfv /home/vagrant/download/jre-8u51-linux-i586.tar.gz -C /home/vagrant/download
+
+sudo mkdir /usr/java
+
+sudo mv /home/vagrant/download/jre1.8.0_51 /usr/java
+
+sudo ln -s /usr/java/jre1.8.0_51/bin/java /usr/bin/java
+
+sudo curl -SL http://download-aws.ej-technologies.com/install4j/install4j_unix_5_1_15.sh -o /home/vagrant/download/install4j_unix_5_1_15.sh
+
+sudo chown -R vagrant:vagrant /home/vagrant/
