775d2489e37c2f6de468829e360bebbd0ed6fd98
yuanho <yuanho@ucar.edu>
2015-08-15 21:01:13 -1000

    jim bug fixed when the parameter is derived of derived

--- src/ucar/unidata/idv/control/DisplayControlImpl.java
+++ src/ucar/unidata/idv/control/DisplayControlImpl.java
@@ -3967,6 +3967,9 @@ public abstract class DisplayControlImpl extends DisplayControlBase implements D
                         String rtime = (String)this.getDataChoice().getProperty("RUNTIME");
                         if(rtime == null && this.getDataChoice() instanceof DerivedDataChoice){
                             DataChoice childChoice = (DataChoice)((DerivedDataChoice) this.getDataChoice()).getChoices().get(0);
+                            while(childChoice instanceof DerivedDataChoice){
+                                childChoice = (DataChoice)((DerivedDataChoice) childChoice).getChoices().get(0);
+                            }
                             rtime = (String)childChoice.getProperty("RUNTIME");
                         }
                         if(rtime != null && rtime.length() > 0)
