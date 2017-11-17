41fa880819eb56cc11e48d48d2ff74acd162ae43
Cece Hedrick <cecehedrick@gmail.com>
2017-11-17 15:28:24 -0700

    fixes color bar race condition bug

--- src/ucar/unidata/idv/control/ColorScaleDialog.java
+++ src/ucar/unidata/idv/control/ColorScaleDialog.java
@@ -2,31 +2,23 @@
  * Copyright 1997-2017 Unidata Program Center/University Corporation for
  * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
  * support@unidata.ucar.edu.
- * 
+ *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or (at
  * your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package ucar.unidata.idv.control;
-
-
-import ucar.unidata.ui.FontSelector;
-import ucar.unidata.util.GuiUtils;
-import ucar.unidata.util.LogUtil;
-
-import ucar.visad.display.ColorScaleInfo;
-
 
 import java.awt.Component;
 import java.awt.Font;
@@ -40,6 +32,11 @@ import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 
+import ucar.unidata.ui.FontSelector;
+import ucar.unidata.util.GuiUtils;
+import ucar.unidata.util.LogUtil;
+
+import ucar.visad.display.ColorScaleInfo;
 
 /**
  * A JFrame widget to get color scale info from the user.
@@ -50,6 +47,13 @@ import javax.swing.JPanel;
  * @author Unidata Development Team
  */
 public class ColorScaleDialog implements ActionListener {
+
+    /** list of orienations */
+    private final static String[] positions = new String[] { ColorScaleInfo.TOP, ColorScaleInfo.BOTTOM,
+                                                             ColorScaleInfo.LEFT, ColorScaleInfo.RIGHT };
+
+    /** The Font selector */
+    private FontSelector fontSelector = null;
 
     /** Was the setting successful */
     private boolean ok;
@@ -86,15 +90,6 @@ public class ColorScaleDialog implements ActionListener {
 
     /** The display */
     private DisplayControlImpl displayControl;
-
-    /** The Font selector */
-    private FontSelector fontSelector = null;
-
-    /** list of orienations */
-    private final static String[] positions = new String[] {
-                                                  ColorScaleInfo.TOP,
-            ColorScaleInfo.BOTTOM, ColorScaleInfo.LEFT,
-            ColorScaleInfo.RIGHT };
 
     /**
      * Construct the widget.
@@ -135,8 +130,6 @@ public class ColorScaleDialog implements ActionListener {
         if (showDialog) {
             dialog.setVisible(true);
         }
-
-
     }
 
     /**
@@ -182,6 +175,15 @@ public class ColorScaleDialog implements ActionListener {
                 displayControl.setColorScaleInfo(
                     new ColorScaleInfo(getInfo()));
             }
+            // A HACK to fix a color bar race condition
+            // To be replaced when race condition is better understood
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
             return true;
         } catch (Exception exc) {
             LogUtil.logException("Setting color scale info", exc);
