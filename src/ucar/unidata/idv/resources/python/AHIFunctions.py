4842a7af0dc0b0f1e08545eda96d215eea33bf61
yuanho <yuanho@ucar.edu>
2019-10-31 15:03:57 -0600

    adding more AHI derived products

--- src/ucar/unidata/idv/resources/python/AHIFunctions.py
+++ src/ucar/unidata/idv/resources/python/AHIFunctions.py
@@ -18,6 +18,26 @@ def AHINightMicrophysicsRGB(b7T, b13T, b15T):
     blu = rescale(b13T, 243, 293, 0, 255)
     return combineRGB(red, grn, blu)
 
+# AHI Day Microphysics Winter RGB
+def AHIWinterDayMicrophysicsRGB(b4A, b7T, b13T):
+    # red = band4; 0% to 100% rescalled to 0 to 255
+    # grn = band7; 0% to 25% rescalled to 0 to 255; gamma 1.5
+    # blu = band13; 203K to 323K rescaled to 0 to 255
+    red = rescale(b4A, 0, 100, 0, 255)
+    grn = 255*(rescale(b7T, 0, 25, 0, 1)**0.6667)
+    blu = rescale(b13T, 203, 323, 0, 255)
+    return combineRGB(red, grn, blu)
+
+# AHI Day Microphysics Summer RGB
+def AHISummerDayMicrophysicsRGB(b4A, b7T, b13T):
+    # red = band4; 0% to 100% rescalled to 0 to 255
+    # grn = band7 0% to 60% rescalled to 0 to 255; gamma 2.5
+    # blu = band13; 203K to 323K rescaled to 0 to 255
+    red = rescale(b4A, 0, 100, 0, 255)
+    grn = 255*(rescale(b7T, 0, 60, 0, 1)**0.4)
+    blu = rescale(b13T, 203, 323, 0, 255)
+    return combineRGB(red, grn, blu)
+
 # AHI Day Convective Storm RGB
 def AHIDayConvectiveStormRGB(b3A, b5A, b7T, b8T, b10T, b13T):
     # red = band8 - band10; -35K to 5K rescaled to 0 to 255
@@ -26,6 +46,36 @@ def AHIDayConvectiveStormRGB(b3A, b5A, b7T, b8T, b10T, b13T):
     red = rescale(b8T-b10T, -35, 5, 0, 255)
     grn = 255*(rescale(b7T-b13T, -5, 60, 0, 1)**2)
     blu = rescale(b5A-b3A, -75, 25, 0, 255)
+    return combineRGB(red, grn, blu)
+
+# AHI Clouds Convection RGB
+def AHICloudsConvectionRGB(b4A, b13T):
+    # red = band4; 0% to 100% rescalled to 0 to 255
+    # grn = band4; 0% to 100% rescalled to 0 to 255
+    # blu = band13; 203K to 323K rescaled to 0 to 255
+    red = rescale(b4A, 0, 100, 0, 255)
+    grn = rescale(b4A, 0, 100, 0, 255)
+    blu = rescale(b13T, 203, 323, 0, 255)
+    return combineRGB(red, grn, blu)
+
+# AHI Severe Storms RGB
+def AHISevereStormRGB(b4A, b7T, b13T):
+    # red = band4; 0% to 100% rescalled to 0 to 255
+    # grn = band4; 0% to 100% rescalled to 0 to 255
+    # blu = band13 - band7; -60K to 40K rescaled to 0 to 255; gamma 2.0
+    red = rescale(b4A, 0, 100, 0, 255)
+    grn = rescale(b4A, 0, 100, 0, 255)
+    blu = 255*(rescale(b13T-b7T, -60, -40, 0, 1)**0.5)
+    return combineRGB(red, grn, blu)
+
+# AHI Day Sonw Fog RGB
+def AHIDaySnowFogRGB(b4A, b5A, b7T):
+    # red = band4; 0% to 100% rescalled to 0 to 255; gamma 1.7
+    # grn = band5; 0% to 70% rescalled to 0 to 255; gamma 1.7
+    # blu = band7; 0% to 30% rescalled to 0 to 255; gamma 1.7
+    red = 255*(rescale(b4A, 0, 100, 0, 1)**0.5882)
+    grn = 255*(rescale(b5A, 0, 70, 0, 1)**0.5882)
+    blu = 255*(rescale(b7T, 0, 30, 0, 1)**0.5882)
     return combineRGB(red, grn, blu)
 
 # AHI Airmass RGB
