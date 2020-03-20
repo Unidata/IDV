48e31272977dc318abd623f6042d79d0cf6b2284
yuanho <yuanho@ucar.edu>
2020-03-20 16:31:53 -0600

    TypedDatasetFactory to FeatureDatasetFactoryManager

--- src/ucar/unidata/data/sounding/CdmTrackAdapter.java
+++ src/ucar/unidata/data/sounding/CdmTrackAdapter.java
@@ -24,54 +24,19 @@ package ucar.unidata.data.sounding;
 
 
 
-import ucar.ma2.Array;
-import ucar.ma2.Index;
-import ucar.ma2.Index0D;
-import ucar.ma2.Range;
-import ucar.ma2.StructureData;
-import ucar.ma2.StructureMembers;
-
-import ucar.nc2.VariableSimpleIF;
 import ucar.nc2.constants.FeatureType;
-import ucar.nc2.dt.TrajectoryObsDataset;
-import ucar.nc2.dt.TrajectoryObsDatatype;
-import ucar.nc2.dt.TypedDataset;
-import ucar.nc2.dt.TypedDatasetFactory;
 
-import ucar.nc2.dt.trajectory.TrajectoryObsDatasetFactory;
+import ucar.nc2.ft.FeatureDatasetFactoryManager;
+import ucar.nc2.ft.FeatureDatasetPoint;
+import ucar.nc2.ft.TrajectoryFeature;
 
+import ucar.nc2.ft.point.standard.StandardTrajectoryCollectionImpl;
+import ucar.nc2.ft.point.standard.TableAnalyzer;
 import ucar.unidata.data.BadDataException;
 
-import ucar.unidata.data.DataAlias;
-import ucar.unidata.data.DataUtil;
-
-import ucar.unidata.data.point.*;
-import ucar.unidata.geoloc.Bearing;
-
-import ucar.unidata.util.JobManager;
-import ucar.unidata.util.Misc;
-import ucar.unidata.util.Trace;
-
-import ucar.visad.UtcDate;
-
-import ucar.visad.Util;
-import ucar.visad.quantities.*;
-
-import visad.*;
-
-import visad.georef.*;
-
-import visad.util.DataUtility;
 
 
-import java.awt.event.*;
-
-import java.rmi.RemoteException;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Hashtable;
-import java.util.List;
+import java.util.*;
 
 
 /**
@@ -89,19 +54,14 @@ public class CdmTrackAdapter extends TrackAdapter {
      * Construct a new track from the filename
      *
      *
-     * @param dataSource _more_
-     * @param filename  location of file
-     * @param pointDataFilter _more_
-     * @param stride _more_
-     * @param lastNMinutes _more_
-     *
      * @throws Exception    On badness
      */
-    //    public CdmTrackAdapter(String filename) throws Exception {
+     public CdmTrackAdapter( ) throws Exception {
     //        super(filename);
-    //    }
+     }
 
 
+    /**
     /**
      * Construct a new track from the filename
      *
@@ -117,27 +77,31 @@ public class CdmTrackAdapter extends TrackAdapter {
                            int lastNMinutes)
             throws Exception {
         super(dataSource, filename, pointDataFilter, stride, lastNMinutes);
-        TrajectoryObsDataset tod = null;
-        StringBuilder         buf = new StringBuilder();
+        FeatureDatasetPoint tod = null;
+        Formatter         buf = new Formatter();
         try {
             //tod = TrajectoryObsDatasetFactory.open(filename);
-            tod = (TrajectoryObsDataset) TypedDatasetFactory.open(
+            tod = (FeatureDatasetPoint) FeatureDatasetFactoryManager.open(
                 FeatureType.TRAJECTORY, filename, null, buf);
         } catch (java.io.FileNotFoundException fnfe) {
             throw new BadDataException("Could not open data source:"
                                        + filename);
         }
 
-        if (tod == null) {
+        if (tod.getFeatureType() == null) {
             throw new BadDataException("Could not open trajectory file:"
                                        + filename);
         }
 
-        List trajectories = tod.getTrajectories();
-        for (int i = 0; i < trajectories.size(); i++) {
-            addTrackInfo(
-                new CdmTrackInfo(
-                    this, tod, (TrajectoryObsDatatype) trajectories.get(i)));
+        List trajectories = tod.getPointFeatureCollectionList();
+        Iterator  itor = trajectories.iterator();
+        while(itor.hasNext()) {
+            StandardTrajectoryCollectionImpl trajectoryFeatureCollection = (StandardTrajectoryCollectionImpl)itor.next();
+            for (TrajectoryFeature trajectoryFeature : trajectoryFeatureCollection) {
+                addTrackInfo(
+                        new CdmTrackInfo(
+                                this, tod, trajectoryFeature));
+            }
         }
     }
 
