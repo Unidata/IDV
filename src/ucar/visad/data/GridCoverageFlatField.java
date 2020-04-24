e5af4bf2f086a356668a43611749009207d038b6
yuanho <yuanho@ucar.edu>
2020-04-24 10:26:33 -0600

    adding logic for the discontiguous interval subsetting, it is not working yet waiting for the nc library

--- src/ucar/visad/data/GridCoverageFlatField.java
+++ src/ucar/visad/data/GridCoverageFlatField.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 1997-2020 Unidata Program Center/University Corporation for
+ * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
+ * support@unidata.ucar.edu.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 package ucar.visad.data;
 
 
@@ -241,11 +261,33 @@ public class GridCoverageFlatField extends CachedFlatField {
                     CoverageCoordAxis1D cca = (CoverageCoordAxis1D)geoGrid.getCoordSys().getTimeAxis();
                     Object [] ttt = cca.getCoordValueNames().toArray();
                     NamedAnything anything = (NamedAnything)ttt[timeIndex];
-                    CalendarDate cdate = (CalendarDate)anything.getValue();
+                    CalendarDate cdate;
+                    CalendarDate cdate1 = null;
+                    if(anything.getValue() instanceof CalendarDate)
+                       cdate = (CalendarDate)anything.getValue();
+                    else if(anything.getValue() instanceof CoordInterval){
+                        double [] od = (double [])cca.getCoordObject(timeIndex);
+                        cdate = cca.makeDate(od[0]);
+                        cdate1 = cca.makeDate(od[1]);
+                    } else
+                        cdate = null;
                     CalendarDateRange subsettimes = CalendarDateRange.of(cdate, cdate);
-                    double [] levels = geoGrid.getCoordSys().getZAxis().getValues();
+                    if(cdate1 != null)
+                        subsettimes = CalendarDateRange.of(cdate, cdate1);
+                    double [] levels = null;
+                    double [] ilevels = null;
+                    if(geoGrid.getCoordSys().getZAxis() != null) {
+                        Array lst = geoGrid.getCoordSys().getZAxis().getCoordsAsArray();
+                        int size = (int) lst.getSize();
+                        levels = new double[size];
+                        for (int k = 0; k <size; k++) {
+                            float kValue = (float) lst.getFloat(k); //.getCoordValue(k);
+                            levels[k] = kValue;
+                        }
+                        ilevels = new double[levels.length];
+                    }
                     double [] vIntv = this.vIntv;
-                    double [] ilevels = new double[levels.length];
+
                     if(vIntv == null)
                         numLevels = 1;
                     else {
