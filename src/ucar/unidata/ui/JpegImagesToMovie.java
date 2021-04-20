fb005aa3f05189effc8c3be38666add840dbb340
yuanho <yuanho@ucar.edu>
2021-04-20 15:10:52 -0600

    this class creates a jpeg base movie and the movie quality is not very good. I copied a class PngImageSourceStream here that can create a png based movie with much better quality, but the resulting movie may not be working in some players.

--- src/ucar/unidata/ui/JpegImagesToMovie.java
+++ src/ucar/unidata/ui/JpegImagesToMovie.java
@@ -28,14 +28,16 @@ import ucar.unidata.util.LogUtil;
 import java.awt.Dimension;
 
 
-
+import java.awt.image.BufferedImage;
 import java.io.*;
 
 import java.util.*;
 
+import javax.imageio.ImageIO;
 import javax.media.*;
 import javax.media.control.*;
 import javax.media.datasink.*;
+import javax.media.format.RGBFormat;
 import javax.media.format.VideoFormat;
 import javax.media.protocol.*;
 import javax.media.protocol.DataSource;
@@ -476,6 +478,8 @@ public class JpegImagesToMovie implements ControllerListener,
             streams = new ImageSourceStream[1];
             streams[0] = new ImageSourceStream(width, height, frameRate,
                     images);
+            //streams[0] = new PngImageSourceStream(width, height, frameRate,
+            //        images);
         }
 
         /**
@@ -559,6 +563,91 @@ public class JpegImagesToMovie implements ControllerListener,
         }
     }
 
+    /*
+     * used png format images to create RGB video format movie, the result movie not
+     * work in PPT and some players
+    */
+    class PngImageSourceStream extends ImageSourceStream {
+
+        public PngImageSourceStream(int width, int height, int frameRate,
+                                    Vector<String> images) {
+            super(width, height, frameRate, images);
+
+            // configure the new format as RGB format
+            format = new RGBFormat(new Dimension(width, height),
+                    Format.NOT_SPECIFIED, Format.byteArray, frameRate,
+                    24, // 24 bits per pixel
+                    1, 2, 3); // red, green and blue masks when data are in the form of byte[]
+        }
+
+        public void read(Buffer buf) throws IOException {
+
+            // Check if we've finished all the frames.
+            if (nextImage >= images.size()) {
+                // We are done. Set EndOfMedia.
+                System.err.println("Done reading all images.");
+                buf.setEOM(true);
+                buf.setOffset(0);
+                buf.setLength(0);
+                ended = true;
+                return;
+            }
+
+            String imageFile = (String) images.elementAt(nextImage);
+            nextImage++;
+
+            System.err.println("  - reading image file: " + imageFile);
+
+            // read the PNG image
+            BufferedImage image = ImageIO.read(new File(imageFile));
+            boolean hasAlpha = image.getColorModel().hasAlpha();
+            Dimension size = format.getSize();
+
+            // convert 32-bit RGBA to 24-bit RGB
+            byte[] imageData = convertTo24Bit(hasAlpha, image.getRaster().getPixels(0, 0, size.width, size.height, (int[]) null));
+            buf.setData(imageData);
+
+            System.err.println("    read " + imageData.length + " bytes.");
+
+            buf.setOffset(0);
+            buf.setLength(imageData.length);
+            buf.setFormat(format);
+            buf.setFlags(buf.getFlags() | Buffer.FLAG_KEY_FRAME);
+        }
+
+        private void convertIntByteToByte(int[] src, int srcIndex, byte[] out, int outIndex) {
+            // Note: the int[] returned by bufferedImage.getRaster().getPixels()
+            // is an int[]
+            // where each int is the value for one color i.e. the first 4 ints
+            // contain the RGBA values for the first pixel
+            int r = src[srcIndex];
+            int g = src[srcIndex + 1];
+            int b = src[srcIndex + 2];
+
+            out[outIndex] = (byte) (r & 0xFF);
+            out[outIndex + 1] = (byte) (g & 0xFF);
+            out[outIndex + 2] = (byte) (b & 0xFF);
+        }
+
+        private byte[] convertTo24Bit(boolean hasAlpha, int[] input) {
+            int dataLength = input.length;
+            int newSize = (hasAlpha ? dataLength * 3 / 4 : dataLength);
+            byte[] convertedData = new byte[newSize];
+
+            // for every 4 int values of the original array (RGBA) write 3
+            // bytes (RGB) to the output array
+            // if there is no alpha (i.e. RGB image) then just convert int to byte
+            for (int i = 0, j = 0; i < dataLength; i += 3, j += 3) {
+                convertIntByteToByte(input, i, convertedData, j);
+                if (hasAlpha) {
+                    i++; // skip an extra byte if the original image has an
+                    // extra int for transparency
+                }
+            }
+            return convertedData;
+        }
+
+    }
 
     /**
      * The source stream to go along with ImageDataSource.
@@ -639,6 +728,10 @@ public class JpegImagesToMovie implements ControllerListener,
                 imageFile = ImageUtils.convertImageTo(imageFile, "jpg");
                 convertedToJpg = true;
             }
+            //if ( !imageFile.endsWith("png")) {
+            //    imageFile = ImageUtils.convertImageTo(imageFile, "png");
+            //    convertedToJpg = true;
+            //}
 
             nextImage++;
 
