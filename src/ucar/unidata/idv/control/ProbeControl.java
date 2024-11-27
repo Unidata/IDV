9cc088b3831aff357a5ebcb81728eea1dcabc874
yuanho <yuanho@ucar.edu>
2024-11-27 12:20:32 -0700

    need to find out a way to auto adjust rnage axis, somehow it will freeze after a few position changed

--- src/ucar/unidata/idv/control/ProbeControl.java
+++ src/ucar/unidata/idv/control/ProbeControl.java
@@ -28,6 +28,8 @@ import ucar.unidata.data.grid.GridUtil;
 import ucar.unidata.data.point.PointDataInstance;
 import ucar.unidata.data.point.PointOb;
 import ucar.unidata.data.point.PointObFactory;
+import ucar.unidata.data.point.PointObTuple;
+import ucar.unidata.data.storm.StormTrackPoint;
 import ucar.unidata.geoloc.LatLonPointImpl;
 import ucar.unidata.geoloc.LatLonRect;
 import ucar.unidata.idv.control.chart.LineState;
@@ -45,6 +47,8 @@ import ucar.unidata.view.geoloc.NavigatedDisplay;
 
 import ucar.visad.ShapeUtility;
 import ucar.visad.Util;
+import ucar.visad.data.CalendarDateTime;
+import ucar.visad.data.CalendarDateTimeSet;
 import ucar.visad.display.Animation;
 import ucar.visad.display.DisplayableData;
 import ucar.visad.display.PointProbe;
@@ -56,6 +60,7 @@ import visad.*;
 import visad.georef.EarthLocation;
 import visad.georef.EarthLocationTuple;
 import visad.georef.LatLonPoint;
+import visad.util.DataUtility;
 
 import java.awt.Component;
 import java.awt.Container;
@@ -2024,6 +2029,10 @@ public class ProbeControl extends DisplayControlImpl implements DisplayableData
         if (rowInfo.isPoint()) {
             String stationName = rowInfo.getStationName();
             if ((stationName != null) && (stationName.length() > 0)) {
+                if(rowInfo.getPointParameter() == null)
+                    try{
+                        rowInfo.setPointParameter(rowInfo.toString());
+                    } catch (Exception dd){}
                 return rowInfo.getPointParameter() + "@" + stationName;
             } else if(rowInfo.getDataInstance() != null){
                 try {
@@ -2576,6 +2585,56 @@ public class ProbeControl extends DisplayControlImpl implements DisplayableData
                 }
                 sample = PointObFactory.makeTimeSequenceOfPointObs(obs, 0,
                         info.getPointIndex());
+            } else if (info.toString().contains("streamflow")) {
+                FlatField ob0 = (FlatField) pointObs.getSample(0);
+                Tuple tuple = (Tuple)ob0.getSample(0);
+                //RealTuple realTuple = (RealTuple)((Tuple)ob0.getSample(0)).getComponent(1);
+                Data [] datas = ((RealTuple)((Tuple)ob0.getSample(0)).getComponent(1)).getComponents();
+                //((Data)((RealTuple)((Tuple)ob0.getSample(0)).getComponent(1)).getComponents()[0]).getType().toString();
+                int dataIdx = 0;
+                for(Data data: datas) {
+                    String typeName = data.getType().toString();
+                    if(removeUnitString(typeName).equals(info.toString())) {
+                        break;
+                    } else {
+                        dataIdx++;
+                    }
+                }
+                int timeLen = pointObs.getDomainSet().getLength();
+                CalendarDateTimeSet timeSet = (CalendarDateTimeSet)pointObs.getDomainSet();
+                double[][]          timeValues = timeSet.getDoubles(false);
+                int idx = findClosestPointIdx(ob0, elt);
+
+                for(int i = 0; i < timeLen; i++) {
+                    FlatField ob00 = (FlatField) pointObs.getSample(i);
+                    int stLen = ob00.getDomainSet().getLength();
+                    CalendarDateTime dt = new CalendarDateTime(timeValues[0][i],
+                                                               timeSet.getCalendar());
+                   //for(int j = 0; j < stLen; j++) {
+                    int j = idx;
+                         //PointOb tp = (PointOb)ob00.getSample(j) ;
+                    RealTuple locationTpl = (RealTuple)((Tuple)ob00.getSample(j)).getComponent(0);
+                    EarthLocation earthLocation = new EarthLocationTuple((Real)locationTpl.getComponent(0),
+                             (Real)locationTpl.getComponent(1), (Real)locationTpl.getComponent(2));
+                    Data dataTpl = (Data)((Tuple)ob00.getSample(j)).getComponent(1);
+                    PointOb pob = new PointObTuple(earthLocation, dt, dataTpl) ;
+                    obs.add(pob);
+                    //}
+                }
+
+                FunctionType sType =
+                        new FunctionType(((SetType)pointObs.getDomainSet().getType()).getDomain(),
+                                datas[dataIdx].getType());
+                sample = new FieldImpl(sType, pointObs.getDomainSet());
+                for (int i = 0; i < numObs; i++) {
+                    FlatField obi = (FlatField) pointObs.getSample(i);
+                    //Tuple tuplei = (Tuple)obi.getSample(0);
+                    //RealTuple realTuplei = (RealTuple)((Tuple)obi.getSample(0)).getComponent(1);
+                    Data [] dataA = ((RealTuple)((Tuple)obi.getSample(idx)).getComponent(1)).getComponents();
+                    Data data = dataA[dataIdx];
+                    sample.setSample(i, data, false);
+                }
+                //return sample;
             } else {
                 obs.add(pointObs);
                 sample = PointObFactory.makeTimeSequenceOfPointObs(obs, 0, 0);
@@ -2630,6 +2689,36 @@ public class ProbeControl extends DisplayControlImpl implements DisplayableData
         return sample;
     }
 
+    public int findClosestPointIdx(FlatField obs, EarthLocation elt) throws
+            VisADException, RemoteException {
+        int                   size    = obs.getLength();
+        float[][] data = obs.getFloats();
+        double minDistance = 1000;
+        int idx = 0;
+        for (int j = 0; j < size; j++) {
+            float lat = data[0][j];
+            float lon = data[1][j];
+            float alt = data[2][j];
+            EarthLocation   stpLoc   = new EarthLocationTuple(new Real(RealType.Latitude, lat),
+                    new Real(RealType.Longitude, lon), new Real(RealType.Altitude, alt));
+            int[]           selectPt = boxToScreen(earthToBox(elt));
+            int[]           obScreen = boxToScreen(earthToBox(stpLoc));
+            double distance = GuiUtils.distance(obScreen, selectPt);
+            if (distance < minDistance) {
+                idx = j;
+                minDistance  = distance;
+            }
+        }
+        return idx;
+    }
+
+    private static String removeUnitString(String name) {
+        if ((name.indexOf("[") > -1) && (name.indexOf("]") > -1)) {
+            return name.substring(0, name.indexOf("[")).trim();
+        } else {
+            return name;
+        }
+    }
     /**
      * Return the list of html for doing the cursor readout
      *
