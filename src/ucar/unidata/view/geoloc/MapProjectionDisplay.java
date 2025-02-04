6c28d3712f24295e68e5c4e06439d3c4e75a1592
yuanho <yuanho@ucar.edu>
2025-02-04 14:34:39 -0700

    fix the deprecated and removal api

--- src/ucar/unidata/view/geoloc/MapProjectionDisplay.java
+++ src/ucar/unidata/view/geoloc/MapProjectionDisplay.java
@@ -795,7 +795,7 @@ public abstract class MapProjectionDisplay extends NavigatedDisplay {
         for (Double i : increment) {
             EarthLocationTuple elt = new EarthLocationTuple(bottomLat, i, 0);
             double[]           values = newtonLon(elt, 0);
-            Double             d      = new Double(values[0]);
+            Double             d      = Double.valueOf(values[0]);
 
             double             mm     = (base - i) % inc;
 
@@ -984,8 +984,8 @@ public abstract class MapProjectionDisplay extends NavigatedDisplay {
         
         Hashtable<Double, String> labelTable = new Hashtable<Double, String>();
 
-        labelTable.put(new Double(maxmin[0]), labelFormat.format(bottom));
-        labelTable.put(new Double(maxmin[1]), labelFormat.format(top));
+        labelTable.put(Double.valueOf(maxmin[0]), labelFormat.format(bottom));
+        labelTable.put(Double.valueOf(maxmin[1]), labelFormat.format(top));
         scale.setLabelTable(labelTable);
         scale.setTickBase(maxmin[0]);
         
