9a426df441bf0c82c77c0113429df90675583662
yuanho <yuanho@ucar.edu>
2025-07-17 13:59:24 -0600

    more labels

--- src/ucar/unidata/idv/control/CrossSectionControl.java
+++ src/ucar/unidata/idv/control/CrossSectionControl.java
@@ -3258,6 +3258,10 @@ public abstract class CrossSectionControl extends GridDisplayControl implements 
             DEFAULT_PRESSURE_LABELS = new String[] {
                     "1000",  "700", "500", "300", "200", "100"
             };
+        else if(vrange.max >= 10000)
+            DEFAULT_PRESSURE_LABELS = new String[] {
+                    "1000",  "700", "500", "350", "250"
+            };
         else if(vrange.max >= 5000)
             DEFAULT_PRESSURE_LABELS = new String[] {
                     "1000",  "850", "700", "600", "500"
